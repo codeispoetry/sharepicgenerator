d9a326bd4e616060d9aae8f58404717921069377
Thomas Pfeiffer <mail@tomro.se>
2019-11-19 14:21:06 +0100

    add donwload awareness

--- code/build/js/20download.js
+++ code/build/js/20download.js
@@ -1,17 +1,21 @@
 $('#download').click(function(){
-    //$(this).prop("disabled", true);
+    $(this).prop("disabled", true);
+    let description = $(this).html();
+    $(this).html('Augenblick bitte');
 
     let data = draw.svg();
-
+    let format = 'png';
+    
     $.ajax({
         type: "POST",
-        url: 'savesvg.php',
-        data: { svg: data },
-        success: function ( data ){
-          let obj = JSON.parse( data );
-          window.location.href =  'download.php?file=' + obj.basename 
-            + '&pdf=false'
-            + '&width=' + info.originalWidth;
+        url: 'createpic.php',
+        data: { svg: data, format: format, width: info.originalWidth },
+        success: function ( data, textStatus, jqXHR ){
+          let obj = JSON.parse( data ); 
+          $('#download').prop("disabled", false);
+          $('#download').html( description);
+  ;
+          window.location.href =  'download.php?file=' + obj.basename + '&format=' + format;
         }
       });
 })
