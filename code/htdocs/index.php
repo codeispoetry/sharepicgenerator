f853587bba95264f0cc12f93f5cce5b9a4819c24
Thomas Rose <t.rose@seidemann-web.com>
2019-11-20 10:27:26 +0100

    reorganize folders again2

--- code/htdocs/index.php
+++ code/htdocs/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Sharepicgenerator Bayern</title>
-    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
 </head>
 <body>
 
@@ -102,6 +102,6 @@
     <script src="./vendor/jquery-3.4.1.min.js"></script>
     <script src="./vendor/svg.min.js"></script>
     <script src="./vendor/svg.draggable.min.js"></script>
-    <script src="./js/main.min.js"></script>
+    <script src="./assets/js/main.min.js"></script>
 </body>
 </html>
