d28a5ea8ebb3aa88b3d64b0f9cc2aa24c5b5e37f
Thomas Rose <t.rose@seidemann-web.com>
2019-11-21 20:20:25 +0100

    layout improvements

--- code/build/js/55textresize.js
+++
@@ -1,5 +0,0 @@
-$('#textfieldresize').bind('input propertychange', function () {
-    let val = parseInt($(this).val());
-    textfield.size(val);
-    info.size = val;
-})
---
+++ code/dist/cockpit.php
@@ -0,0 +1,78 @@
+<div class="list-group">
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Textgröße</h5>
+            <small>3 days ago</small>
+        </div>
+        <div class="mb-1"><label for="customRange1">Example range</label>
+            <input type="range" class="custom-range" id="customRange1">
+            <div class="slider">
+                <small>klein</small>
+                <input type="range" class="custom-range" id="textsize" min="1" max="100">
+                <small>groß</small>
+            </div>
+        </div>
+        <small>Donec id elit non mi porta.</small>
+    </div>
+
+
+</div>
+
+
+
+
+
+<div id="upload">
+    <span class="message"></span>
+    <input type='file' id="uploadfile" accept='image/*'>
+</div>
+<div>
+    <button id="download">Download</button>
+</div>
+<div>
+    <h6><i class="fas fa-bullhorn"></i>Slogan</h6>
+    <textarea id="text">grün tut gut</textarea><br>
+    <small>Zeilen, die mit einem<br>
+        Ausrufezeichen ! beginnen,<br>
+        werden hervorgehoben</small>
+</div>
+
+<div>
+    <h6>Pin</h6>
+    <div class="slider">
+        <small>klein</small>
+        <input type="range" id="pinsize" min="1" max="100">
+        <small>groß</small>
+    </div>
+</div>
+<div>
+    <h6>Zeile unten</h6>
+    <input type="text" id="subline" value="gruene-bayern.de">
+</div>
+<div>
+    <h6>Größe</h6>
+    <a href="#" id="sizereset">zurücksetzen</a><br>
+    <input type="number" id="width" class="size" step="10"> x
+    <input type="number" id="height" class="size" step="10"> Pixel
+</div>
+<div>
+    <h6>Hintergrundbild</h6>
+    <a href="#" id="backgroundreset">zurücksetzen</a>
+    <div class="slider">
+        <small>klein</small>
+        <input type="range" id="backgroundsize" min="1" max="1500" value="1200">
+        <small>groß</small>
+    </div>
+</div>
+<div>
+    <h6>Pixabay</h6>
+    <button id="pixabayopener">Bilder suchen</button>
+</div>
+<div>
+    <input type="hidden" id="pinX">
+    <input type="hidden" id="pinY">
+    <input type="hidden" id="backgroundX">
+    <input type="hidden" id="backgroundY">
+    <input type="hidden" id="textX">
+    <input type="hidden" id="textY">
+</div>
