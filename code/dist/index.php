9bd4e429e0d4ac636d6581920821b8a1ed787601
Thomas Rose <t.rose@seidemann-web.com>
2019-11-21 20:20:25 +0100

    layout improvements

--- code/dist/index.php
+++ code/dist/index.php
@@ -6,97 +6,47 @@
     <title>Sharepicgenerator Bayern</title>
     <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
 </head>
-<body>
-    <header>
-        <h1>Sharepicgenerator Bayern</h1>
-    </header>
-    <div id="workspace">
-        <div id="workbench">
-            <div id="canvas"></div>
-            <div id="message"></div>
+<body class="">
+<div class="container-fluid h-100">
+    <div class="row">
+        <div class="ml-md-auto">
+            <div class="col-12 text-center">
+                <h1>Sharepicgenerator Bayern</h1>
+            </div>
+            <div class="col-12">
+                <div id="canvas"></div>
+                <div id="message"></div>
+            </div>
         </div>
-        <div id="cockpit">
-            <div id="upload">
-                <span class="message"></span>
-                <input type='file' id="uploadfile" accept='image/*'>
-            </div>
-            <div>
-                <button id="download">Download</button>
-            </div>
-            <div>
-                <h6><i class="fas fa-bullhorn"></i>Slogan</h6>
-                <textarea id="text">grün tut gut</textarea><br>
-                <small>Zeilen, die mit einem<br>
-                    Ausrufezeichen ! beginnen,<br>
-                    werden hervorgehoben</small>
-            </div>
-            <div>
-                <h6>Textgröße</h6>
-                <div class="slider">
-                    <small>klein</small>
-                    <input type="range" id="textsize" min="1" max="100">
-                    <small>groß</small>
-                </div>
-            </div>
-            <div>
-                <h6>Pin</h6>
-                <div class="slider">
-                    <small>klein</small>
-                    <input type="range" id="pinsize" min="1" max="100">
-                    <small>groß</small>
-                </div>
-            </div>
-            <div>
-                <h6>Zeile unten</h6>
-                <input type="text" id="subline" value="gruene-bayern.de">
-            </div>
-            <div>
-                <h6>Größe</h6>
-                <a href="#" id="sizereset">zurücksetzen</a><br>
-                <input type="number" id="width" class="size" step="10"> x
-                <input type="number" id="height" class="size" step="10"> Pixel
-            </div>
-            <div>
-                <h6>Hintergrundbild</h6>
-                <a href="#" id="backgroundreset">zurücksetzen</a>
-                <div class="slider">
-                    <small>klein</small>
-                    <input type="range" id="backgroundsize" min="1" max="1500" value="1200">
-                    <small>groß</small>
-                </div>
-            </div>
-            <div>
-                <h6>Pixabay</h6>
-                <button id="pixabayopener">Bilder suchen</button>
-            </div>
-            <div>
-                <input type="hidden" id="pinX">
-                <input type="hidden" id="pinY">
-                <input type="hidden" id="backgroundX">
-                <input type="hidden" id="backgroundY">
-                <input type="hidden" id="textX">
-                <input type="hidden" id="textY">
-            </div>
+        <div class="ml-md-auto p-3">
+            <?php require_once('cockpit.php'); ?>
         </div>
     </div>
-        <div class="overlays">
-            <div id="pixabay" class="overlay">
-                <a href="#" class="close">schließen</a>
-                <form>
-                    <input type="text" class="q" value="berge">
-                    <input type="submit" i value="suchen">
-                </form>
-                <div class="results"></div>
-            </div>
-            <div id="waiting" class="overlay">
-                Augenblick bitte
-            </div>
-        </div>
+</div>
+
+<footer class="fixed-bottom bg-primary p-2 text-white">foo
+</footer>
 
 
-    <script src="./vendor/jquery-3.4.1.min.js"></script>
-    <script src="./vendor/svg.min.js"></script>
-    <script src="./vendor/svg.draggable.min.js"></script>
-    <script src="./assets/js/main.min.js"></script>
+
+
+<div class="overlays">
+    <div id="pixabay" class="overlay">
+        <a href="#" class="close">schließen</a>
+        <form>
+            <input type="text" class="q" value="berge">
+            <input type="submit" i value="suchen">
+        </form>
+        <div class="results"></div>
+    </div>
+    <div id="waiting" class="overlay">
+        Augenblick bitte
+    </div>
+</div>
+
+<script src="./vendor/jquery-3.4.1.min.js"></script>
+<script src="./vendor/svg.min.js"></script>
+<script src="./vendor/svg.draggable.min.js"></script>
+<script src="./assets/js/main.min.js"></script>
 </body>
 </html>
