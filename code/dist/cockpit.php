bd1773a038ab89b4e397761df0645fe7b2d55da7
Thomas Rose <t.rose@seidemann-web.com>
2019-11-21 20:58:11 +0100

    change cockpit layout

--- code/dist/cockpit.php
+++ code/dist/cockpit.php
@@ -14,7 +14,7 @@
         <small></small>
     </div>
 
- <div class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Pin</h5>
             <small></small>
@@ -29,52 +29,99 @@
         <small></small>
     </div>
 
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Text</h5>
+            <small></small>
+        </div>
+        <div class="mb-1">
+            <textarea id="text" class="form-control">grün tut gut</textarea>
+        </div>
+        <small>Zeilen, die mit einem
+            Ausrufezeichen ! beginnen,
+            werden hervorgehoben</small>
+    </div>
+
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Zeile unten</h5>
+            <small></small>
+        </div>
+        <div class="mb-1">
+            <input type="text" id="subline" value="gruene-bayern.de" class="form-control">
+        </div>
+        <small></small>
+    </div>
+
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Bildgröße</h5>
+            <small></small>
+        </div>
+        <div class="mb-1">
+            <div class="form-inline">
+                <input type="number" class="form-control size" id="width" step="10"><span class="m-1">x</span>
+                <input type="number" class="form-control size mr-1" id="height" step="10"> Pixel
+            </div>
+            <div class="text-secondary cursor-pointer" id="sizereset">zurücksetzen</div>
+        </div>
+        <small></small>
+    </div>
+
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Hintergrundbild</h5>
+            <small></small>
+        </div>
+        <div class="mb-1">
+
+            <div class="slider">
+                <small>klein</small>
+                <input type="range" class="custom-range" id="backgroundsize" min="1" max="1500" value="1200">
+                <small>groß</small>
+            </div>
+            <div class="text-secondary cursor-pointer" id="backgroundreset">zurücksetzen</div>
+        </div>
+        <small></small>
+    </div>
+
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Pixabay</h5>
+            <small></small>
+        </div>
+        <div class="mb-1">
+            <a href="#" class="text-secondary cursor-pointer" id="pixabayopener">Bilder suchen</a>
+        </div>
+        <small></small>
+    </div>
+
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Eigenes BIld</h5>
+            <small></small>
+        </div>
+        <div class="mb-1">
+            <div class="custom-file" id="upload">
+                <input type="file" class="custom-file-input" id="uploadfile" accept="image/*">
+                <label class="custom-file-label" for="uploadfile">Datei wählen</label>
+                <div class="message"></div>
+            </div>
+        </div>
+        <small></small>
+    </div>
+
 
 </div>
 
 
 
-
-
-<div id="upload">
-    <span class="message"></span>
-    <input type='file' id="uploadfile" accept='image/*'>
-</div>
 <div>
     <button id="download">Download</button>
 </div>
-<div>
-    <h6><i class="fas fa-bullhorn"></i>Slogan</h6>
-    <textarea id="text">grün tut gut</textarea><br>
-    <small>Zeilen, die mit einem<br>
-        Ausrufezeichen ! beginnen,<br>
-        werden hervorgehoben</small>
-</div>
 
 
-<div>
-    <h6>Zeile unten</h6>
-    <input type="text" id="subline" value="gruene-bayern.de">
-</div>
-<div>
-    <h6>Größe</h6>
-    <a href="#" id="sizereset">zurücksetzen</a><br>
-    <input type="number" id="width" class="size" step="10"> x
-    <input type="number" id="height" class="size" step="10"> Pixel
-</div>
-<div>
-    <h6>Hintergrundbild</h6>
-    <a href="#" id="backgroundreset">zurücksetzen</a>
-    <div class="slider">
-        <small>klein</small>
-        <input type="range" id="backgroundsize" min="1" max="1500" value="1200">
-        <small>groß</small>
-    </div>
-</div>
-<div>
-    <h6>Pixabay</h6>
-    <button id="pixabayopener">Bilder suchen</button>
-</div>
+
 <div>
     <input type="hidden" id="pinX">
     <input type="hidden" id="pinY">
