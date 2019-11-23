c98a23ed76ff4ad114047094db07c5635bd6a085
Thomas Pfeiffer <mail@tomro.se>
2019-11-23 22:50:06 +0100

    fix deployment in webserver container

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,3 +1,3 @@
 
-rm code/dist/tmp/*
-rsync -avhz code/dist/ tom@sharepicgenerator.de:/var/www/html/by2020 --delete
+rm /var/www/html/dist/tmp/*
+rsync -avhz /var/www/html/dist/ tom@sharepicgenerator.de:/var/www/html/by2020 --delete
