380528e021a1206693254edf7e60fedfd0482fde
Thomas Rose <t.rose@seidemann-web.com>
2019-12-12 18:21:50 +0100

    get changes from bayern

---
+++ code/dist/telegram/sendinfo.php
@@ -0,0 +1,39 @@
+<?php
+
+if (file_exists('../config.ini')) {
+    $keys = parse_ini_file('../config.ini', TRUE);
+
+    $botID = $keys["Telegram"]["botID"];
+    $chatID = $keys["Telegram"]["channelID"];
+
+    sendFile('tmp/' . $filename . '.' . $format);
+}
+
+
+function sendMessage($message)
+{
+    if (!$message) return;
+    global $botID, $chatID;
+    readfile('https://api.telegram.org/' . $botID . '/sendMessage?chat_id=' . $chatID . '&text=' . $message);
+}
+
+function sendFile($file)
+{
+    global $botID, $chatID;
+    $botURL = "https://api.telegram.org/$botID/";
+    $url = $botURL . "sendPhoto?chat_id=" . $chatID;
+
+    $post_fields = array('chat_id' => $chatID,
+        'photo' => new CURLFile(realpath($file)),
+        'silent' => true
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-Type:multipart/form-data"
+    ));
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+    $output = curl_exec($ch);
+}
