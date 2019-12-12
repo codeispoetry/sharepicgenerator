a08b78029885de5046ce664ce966245eee0c2915
Thomas Rose <t.rose@seidemann-web.com>
2019-12-12 18:48:21 +0100

    add yellow textparts in main text

--- code/build/js/51textbund.js
+++ code/build/js/51textbund.js
@@ -2,13 +2,14 @@ const text = {
     svg: draw.text(''),
     colors: ['#ffffff', '#ffee00'],
     fontsizes: [20, 20],
-    lineheights: [20, 20],
+    lineheight: 20,
     yBiases: [0, 0],
     linemargin: 0,
     paddingLr: 5,
     font: {
         anchor: 'left',
-        leading: '1.0em'
+        leading: '1.0em',
+        size: 20
     },
     fontoutsidelines: {
         family: 'ArvoGruen',
@@ -29,48 +30,57 @@ const text = {
 
         let y = 0;
         let lines = $('#text').val().split(/\n/);
-        let fontfamily = (lines.length <= 3 ) ? 'ArvoGruen' : 'Arvo';
-      
-        lines.forEach(function (value, index, array) {
-                let style = /^!/.test(value) ? 1 : 0;
-                 
-                value = value.replace(/^!/, '').toUpperCase();
+        let fontfamily = (lines.length <= 3) ? 'ArvoGruen' : 'Arvo';
 
-                let t = draw.text(value).font({...text.font, ...{family: fontfamily, size: text.fontsizes[style]}}).fill(text.colors[style]).move(0, y + text.yBiases[style]);
+        lines.forEach(function (value, index, array) {
+
+                let style = 1;
+
+                values = value.toUpperCase().split(/\[|\]/);
+
+                let t = draw.text(function (add) {
+                    for(let i = 0; i<values.length; i++) {
+                        style = (style == 0 ) ? 1 : 0;
+                        console.log(style);
+                        add.tspan( values[i] ).fill(text.colors[style]).font({...text.font, ...{family: fontfamily}});
+                    }
+                });
+
+                t.move(0, y + text.yBiases[style]);
 
                 text.svg.add(t);
-                y += text.lineheights[style] + text.linemargin;
+                y += text.lineheight + text.linemargin;
             }
         );
 
         // add upper and lower line
-        let linebefore = draw.rect( text.svg.width(), 2).fill( '#ffffff').dy( -4 );
-        let lineafter  = linebefore.clone().dy( text.svg.height() + 6 );
-        text.svg.add( linebefore);
-        text.svg.add( lineafter );
+        let linebefore = draw.rect(text.svg.width(), 2).fill('#ffffff').dy(-4);
+        let lineafter = linebefore.clone().dy(text.svg.height() + 6);
+        text.svg.add(linebefore);
+        text.svg.add(lineafter);
 
         // text above and below the line
-        let textbefore = draw.text( $('#textbefore').val() ).font( text.fontoutsidelines ).fill('#ffffff').dy(-14);
-        let textafter = draw.text( $('#textafter').val() ).font( text.fontoutsidelines ).fill('#ffffff').dy( text.svg.height() - 2 );
-        text.svg.add( textbefore );
-        text.svg.add( textafter );
+        let textbefore = draw.text($('#textbefore').val()).font(text.fontoutsidelines).fill('#ffffff').dy(-14);
+        let textafter = draw.text($('#textafter').val()).font(text.fontoutsidelines).fill('#ffffff').dy(text.svg.height() - 2);
+        text.svg.add(textbefore);
+        text.svg.add(textafter);
 
 
         // green background behind text
-        if($('#design').val() == 'textbackground'){
+        if ($('#design').val() == 'textbackground') {
             let textbackgroundpadding = 10;
-            let textbackground = draw.rect( text.svg.width() + 2 * textbackgroundpadding, text.svg.height() + 2 * textbackgroundpadding ).fill('#46962b').move(-textbackgroundpadding  , -14).back();
+            let textbackground = draw.rect(text.svg.width() + 2 * textbackgroundpadding, text.svg.height() + 2 * textbackgroundpadding).fill('#46962b').move(-textbackgroundpadding, -14).back();
 
-            if($('#textbefore').val()){
-                textbackground.dy( -6 );
+            if ($('#textbefore').val()) {
+                textbackground.dy(-6);
             }
 
-            if($('#textbefore').val() && $('#textafter').val() ){
-              textbackground.height( textbackground.height() - 6 );
+            if ($('#textbefore').val() && $('#textafter').val()) {
+                textbackground.height(textbackground.height() - 6);
             }
 
             text.svg.add(textbackground);
-            textbackground.back(); 
+            textbackground.back();
         }
 
         text.svg.move(parseInt($('#textX').val()), parseInt($('#textY').val())).size(parseInt($('#textsize').val()));
