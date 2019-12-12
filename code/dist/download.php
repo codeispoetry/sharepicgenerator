e58e02fdc6c482104e204c97c607c803d35ec890
Thomas Rose <t.rose@seidemann-web.com>
2019-12-12 18:21:50 +0100

    get changes from bayern

--- code/dist/download.php
+++ code/dist/download.php
@@ -1,6 +1,6 @@
 <?php
-
 $filename = sanitize_filename($_GET['file']);
+$downloadname = $_GET['downloadname'] ?: 'no-download-name';
 
 $contentType = 'image/png';
 $format = 'png';
@@ -10,16 +10,25 @@ if ($_GET['format'] && $_GET['format'] == 'pdf') {
     $format = 'pdf';
 }
 
+logthis($downloadname);
+
 header('Content-Type: ' . $contentType);
-header('Content-Disposition: attachment; filename="sharepic.' . $format . '"');
+header('Content-Disposition: attachment; filename="' . $downloadname . '.' . $format . '"');
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 readfile('tmp/' . $filename . '.' . $format);
 
-unlink('tmp/' . $filename . '.' . $format);
+require_once('telegram/sendinfo.php');
+
+//unlink('tmp/' . $filename . '.' . $format);
 unlink('tmp/' . $filename . '.svg');
 
+function logthis($filename)
+{
+    $line = sprintf("%s\t%s\t%s\n", time(), $filename, "download");
+    file_put_contents('log/log.txt', $line, FILE_APPEND);
+}
 
 function sanitize_filename($var)
 {
