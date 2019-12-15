996ff8393d24cba1c0fcb43d3785453ec6a32c71
Thomas Pfeiffer <mail@tomro.se>
2019-12-15 11:27:29 +0100

    move icons into layer

--- code/dist/index.php
+++ code/dist/index.php
@@ -92,6 +92,48 @@ if (file_exists($samlfile)) {
 
     </div>
 
+    <div id="iconoverlay" class="overlay">
+        <div class="container-fluid">
+            <a href="#" class="close text-danger">
+                <i class="fas fa-times"></i>
+            </a>
+            <div class="row pt-2 mt-1">
+                <div class="col-12 text-center">
+                    <h2>Icons suchen</h2>
+                </div>
+                <div class="col-4 offset-4" id="pixabay">
+                    <form>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-images"></i></div>
+                            </div>
+                            <input type="text" class="form-control q" placeholder="z.B. Berge oder Sonnenblume">
+                            <div class="input-group-append">
+                                <button type="submit" class="input-group-text btn-primary">Suchen</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-12 p-5 results">
+            <?php
+                $icon_dirs = array_merge(
+                    glob('assets/icons/solid/*.svg'),
+                    glob('assets/icons/regular/*.svg'),
+                    glob('assets/icons/brands/*.svg')
+                );
+
+                foreach( $icon_dirs AS $icon ){
+                    printf('<img src="%s" class="chooseicon"/>', $icon); 
+                }
+            ?>
+            
+            </div>
+        </div>
+
+    </div>
+
 
     <div id="waiting" class="overlay text-danger bg-light">
         <h1>Augenblick bitte</h1>
