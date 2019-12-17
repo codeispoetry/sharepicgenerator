6ea96ce9f7248714055ab80b9086c7c6273bc7b7
Thomas Rose <t.rose@seidemann-web.com>
2019-12-17 11:42:01 +0100

    refactor log-dirs

--- code/dist/telegram/sendinfo.php
+++ code/dist/telegram/sendinfo.php
@@ -1,7 +1,7 @@
 <?php
 
-if (file_exists('../config.ini')) {
-    $keys = parse_ini_file('../config.ini', TRUE);
+if (file_exists('config.ini')) {
+    $keys = parse_ini_file('config.ini', TRUE);
 
     $botID = $keys["Telegram"]["botID"];
     $chatID = $keys["Telegram"]["channelID"];
