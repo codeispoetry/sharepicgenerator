bc4acd469884a6305075733323a78a648894f5a8
Thomas Rose <t.rose@seidemann-web.com>
2019-12-17 13:21:41 +0100

    improve deploy

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,2 +1,2 @@
 
-rsync -avhz --exclude log/log.txt --exclude tmp /var/www/html/dist/ tom@sharepicgenerator.de:/var/www/html/v4 --delete
+rsync -avhz --exclude log/log.txt --exclude log/countsharepics.txt --exclude tmp /var/www/html/dist/ tom@sharepicgenerator.de:/var/www/html/v4 --delete
