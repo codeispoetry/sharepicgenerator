1c6cae2cd43a23cfe8bac93f99daeca444d6867b
Thomas Pfeiffer <mail@tomro.se>
2019-12-18 16:57:08 +0100

    add maketask for deployment

--- Makefile
+++ Makefile
@@ -21,3 +21,6 @@ shell:
 
 down:
 	docker-compose down
+
+deploy:
+	docker-compose exec webserver bash /root/scripts/deploy.sh
--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,2 +1,1 @@
-
 rsync -avhz --exclude log/log.txt --exclude log/countsharepics.txt --exclude tmp /var/www/html/dist/ tom@sharepicgenerator.de:/var/www/html/v4 --delete
