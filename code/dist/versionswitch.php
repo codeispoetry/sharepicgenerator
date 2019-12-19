6254f6b4e078c1f5c48c5adf3cfdd224c39fd957
Thomas Rose <t.rose@seidemann-web.com>
2019-12-19 10:55:21 +0100

    fix bayern number for version switch

--- code/dist/versionswitch.php
+++ code/dist/versionswitch.php
@@ -4,7 +4,7 @@ if (isset($as) ) {
     $attributes = $as->getAttributes();
     $landesverband = substr($attributes['membershipOrganizationKey'][0], 1, 2);
 
-    if ($landesverband == '01' AND isset($_SERVER['HTTP_REFERER'])) {
+    if ($landesverband == '02' AND isset($_SERVER['HTTP_REFERER'])) {
         if ('saml.gruene.de' == parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST)) {
             // freshly logged in
             header('Location: /bayern', true, 302);
