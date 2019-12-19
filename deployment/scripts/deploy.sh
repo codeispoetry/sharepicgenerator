80ec1f217b2ead4b145c19af260a3452da069f98
Thomas Pfeiffer <mail@tomro.se>
2019-12-19 20:35:26 +0100

    change deployment

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,1 +1,1 @@
-rsync -avhz --exclude log/log.txt --exclude log/countsharepics.txt --exclude tmp /var/www/html/dist/ tom@sharepicgenerator.de:/var/www/html/v4 --delete
+rsync -avhz --exclude bayern --exclude youtubedownloader --exclude log/log.txt --exclude log/countsharepics.txt --exclude tmp /var/www/html/dist/ tom@sharepicgenerator.de:/var/www/html --delete
