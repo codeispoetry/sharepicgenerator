5d273bc57fd55ff2a2540506ed89f31a56da438a
Thomas Rose <t.rose@seidemann-web.com>
2019-12-19 10:53:59 +0100

    add versionswitch

---
+++ code/dist/versionswitch.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($as) ) {
+    $attributes = $as->getAttributes();
+    $landesverband = substr($attributes['membershipOrganizationKey'][0], 1, 2);
+
+    if ($landesverband == '01' AND isset($_SERVER['HTTP_REFERER'])) {
+        if ('saml.gruene.de' == parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST)) {
+            // freshly logged in
+            header('Location: /bayern', true, 302);
+            die();
+        }
+    }
+}
