59b9ceb9eb96a2a6b921a8ab96f5e4572984f24e
Thomas Rose <t.rose@seidemann-web.com>
2020-01-14 16:40:06 +0100

    log ffmpeg per thread

--- code/dist/getvideoencodestatus.php
+++ code/dist/getvideoencodestatus.php
@@ -1,6 +1,9 @@
 <?php
 
-exec('tail -n 1 tmp/ffmpeg.log', $output);
+$logfile =  sprintf('tmp/%s.log', basename( $_GET['videofile'], '.mp4' ) );
+
+$command = sprintf('tail -n 1 %s', $logfile);
+exec($command, $output);
 
 if( preg_match_all('/time=(.*?) /',$output[0],$matches)) {
     echo json_encode(array('currentposition' => timecode2seconds(end( $matches[1]))));
