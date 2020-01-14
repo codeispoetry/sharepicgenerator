03495a6cace2cc6504946ea367b354484488234d
Thomas Rose <t.rose@seidemann-web.com>
2020-01-14 14:59:35 +0100

    add percentage in videoencoding

--- code/dist/getvideoencodestatus.php
+++ code/dist/getvideoencodestatus.php
@@ -3,9 +3,23 @@
 exec('tail -n 1 tmp/ffmpeg.log', $output);
 
 if( preg_match_all('/time=(.*?) /',$output[0],$matches)) {
-    echo json_encode(array('currentposition' => end($matches[1])));
+    echo json_encode(array('currentposition' => timecode2seconds(end( $matches[1]))));
     die();
 }else{
     echo json_encode(array('currentposition' => 'end'));
     die();
+}
+
+
+function timecode2seconds( $timecode ){
+    $parts = explode(":",$timecode);
+    $parts = array_reverse($parts);
+    $seconds = 0;
+    $multiplier = 1;
+    foreach($parts AS $part){
+        $seconds += $part * $multiplier;
+        $multiplier *= 60;
+    }
+
+    return $seconds;
 }
