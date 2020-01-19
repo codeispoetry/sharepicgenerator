ae4c8666a04c492f0be43e95b4552e44eea4edff
Thomas Rose <mail@tom-rose.de>
2020-01-19 14:44:35 +0100

    add accesstokens for delete-operations

--- code/dist/delete.php
+++ code/dist/delete.php
@@ -1,10 +1,31 @@
 <?php
 
 if( !isset($_POST['user'] )) die();
+if( !isset($_POST['accesstoken'] )) die();
+
 
 $user = preg_replace('/[^a-zA-Z0-9]/','', $_POST['user']);
+$accesstoken = preg_replace('/[^a-zA-Z0-9]/','', $_POST['accesstoken']);
+
+if( !checkPermission( $user, $accesstoken )){
+    die('-1');
+};
 
 deleteUserLogo( $user );
+
+
+
+function checkPermission( $user, $accesstoken){
+    $userDir = 'persistent/user/' . $user;
+    if( !file_exists($userDir)){
+        return false;
+    }
+
+    return ( file_get_contents( sprintf('%s/accesstoken.php',$userDir)) == $accesstoken);
+}
+
+
+
 
 function deleteUserLogo( $user ){
 
