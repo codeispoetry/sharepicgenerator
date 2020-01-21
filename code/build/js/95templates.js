5aa9b82f259fb70dd33ccc9244e7c2c8909bb087
Thomas Rose <t.rose@seidemann-web.com>
2020-01-21 15:45:57 +0100

    add viewport to template overlay

--- code/build/js/95templates.js
+++ code/build/js/95templates.js
@@ -1,6 +1,8 @@
 $('#templateopener').click(function () {
+    $('head meta[name="viewport"]').attr('content','width=device-width, initial-scale=1');
+
     $('#templates').addClass("active");
-})
+});
 
 
 $('.templatepic').click(function(){
