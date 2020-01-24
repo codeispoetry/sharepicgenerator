221b0944dd1bc0eb662eecb81be243a0e24e7fcb
Thomas Rose <mail@tom-rose.de>
2020-01-24 22:47:45 +0100

    improve logging

--- code/dist/log/do.php
+++ code/dist/log/do.php
@@ -1,3 +1,3 @@
 <?php
-$line = sprintf("%s\t%s\t%s\n", time(), $user, "login" );
+$line = sprintf("%s\t%s\t%s\t%s\n", time(), $user, "login", $landesverband );
 file_put_contents('log/log.log', $line, FILE_APPEND);
