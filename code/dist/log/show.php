7f50317cade46fd07d0b247719584a0cfa6eb810
Thomas Rose <mail@tom-rose.de>
2020-01-25 23:25:55 +0100

    beautify log

--- code/dist/log/show.php
+++ code/dist/log/show.php
@@ -1,35 +1,82 @@
-<style>
-    img{
-        height: 200px;
-        width: auto;
-        margin: 0 5px 5px 0;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="theme-color" content="#46962b">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Logs</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+	<style>
+		 img{
+            height: 200px;
+            width: auto;
+            margin: 0 5px 5px 0;
+            max-width: 50%;
+        }
+        scroll-container {
+            scroll-behavior: smooth;
+        }
+	</style>
+</head>
+<body>
+<scroll-container>
+<div class="container-fluid">
+    <div class="row mt-3">
+		<div class="col-12 text-center">
+            <a href="#bayern" class="btn btn-info btn-lg">zu Bayern</a>
+			<a href="index.php" class="btn btn-primary btn-lg ml-2">Zeige Statistik</a>
+		</div>
 
-<a href="#bayern">zu Bayern</a><br>
+
+        <div class="col-12 text-center">
+            <?php
+                deleteFilesInPathOlderThanDays(2, '../tmp/*'); echo ' | ';
+                deleteFilesInPathOlderThanDays(2, '../bayern/tmp/*'); echo ' | ';
+                deleteFilesInPathOlderThanDays(2, '../vintage/tmp/*');
+            ?>
+        </div>
 
 
+        <div class="col-12 text-center">
+            <a name="videos"><h2>Videos</h2></a>
+            <?php
+                show_videos("../tmp/shpic*\.mp4");
+                show_videos("../bayern/tmp/shpic*\.mp4");
+            ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-center">
+            <a name="vintage"><h2>Vintage</h2></a>
+        </div>
+        <?php
+            show_images("../vintage/tmp/log*\.jpg");
+        ?>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-center">
+            <h2>Bund</h2>
+        </div>
+        <?php
+            show_images("../tmp/log*\.jpg");
+        ?>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-center">
+            <scroll-page id="bayern"><h2>Bayern</h2></scroll-page>
+        </div>
+        <?php
+            show_images("../bayern/tmp/log*\.jpg");
+        ?>
+    </div>
+
+</div>
+</scroll-container>
+</html>
 <?php
-
-deleteFilesInPathOlderThanDays(2, '../tmp/*');
-deleteFilesInPathOlderThanDays(2, '../bayern/tmp/*');
-deleteFilesInPathOlderThanDays(2, '../vintage/tmp/*');
-
-echo '<a name="videos"><h2>Videos</h2></a>';
-show_videos("../tmp/shpic*\.mp4");
-show_videos("../bayern/tmp/shpic*\.mp4");
-
-echo '<a name="vintage"><h2>Vintage</h2></a>';
-show_images("../vintage/tmp/log*\.jpg");
-
-echo '<a name="bund"><h2>Bund</h2></a>';
-show_images("../tmp/log*\.jpg");
-
-echo '<a name="bayern"><h2>Bayern</h2></a>';
-show_images("../bayern/tmp/log*\.jpg");
-
-
-
 
 
 function show_videos($dir)
@@ -48,7 +95,7 @@ function show_images($dir)
     $files = array_reverse(glob( $dir ) );
 
     foreach ($files AS $file) {
-        printf('<img src="%s"/>', $file);
+        printf('<div class="col-6 col-md-3 col-lg-2"><img src="%s" class="img-fluid"/></div>', $file);
     }
 }
 
@@ -66,5 +113,5 @@ function deleteFilesInPathOlderThanDays($days, $path)
         }
     }
 
-    printf('%d Dateien gelöscht.<br>', $counter);
+    printf('%d Dateien gelöscht ', $counter);
 }
