576f68ad2b5569e984886cc8a4ae144b826a00a3
Thomas Rose <t.rose@seidemann-web.com>
2020-01-27 15:29:59 +0100

    add dynamic waiting for api

--- code/dist/api/api.py
+++ code/dist/api/api.py
@@ -8,6 +8,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 
@@ -34,9 +37,11 @@ options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 
 driver = webdriver.Chrome(dir + '/chromedriver', chrome_options=options)
+driver.set_window_size(1400,800)
+
 try:
-    driver.get('https://127.0.0.1/bayern')
-    #driver.get('http://127.0.0.1/create.php')
+    #driver.get('https://127.0.0.1/bayern')
+    driver.get('http://127.0.0.1/create.php')
 except:
     driver.save_screenshot("screenshot.png")
     sys.stderr.write("Could not connect. Screenshot saved.")
@@ -51,12 +56,16 @@ textEl.send_keys( text )
 #sys.exit()
 
 
-try:
+if os.path.isfile( userDir + "/picture.jpg"):
     driver.find_element_by_id("uploadfile").send_keys( userDir + "/picture.jpg")
-    time.sleep(5)
-    print "uploaded ..."
-except:
-    print("No file to upload")
+    print "upload "
+    time.sleep(1)
+    element = WebDriverWait(driver, 20).until(
+        EC.invisibility_of_element((By.ID, "waiting"))
+    )
+    print "uploaded "
+else :
+    print("no file to upload in " + userDir)
 
 
 
@@ -68,19 +77,23 @@ except:
 
 javaScript = "$('#textsize').val(300); $('#textX').val(-100); $('#textY').val(2000); text.draw(); text.bounce();"
 driver.execute_script(javaScript)
-time.sleep(1)
+#time.sleep(1)
 
 # for bayern only
 javaScript = "$('#pinsize').val(85); $('#pinX').val(4000); $('#pinY').val(4000); pin.draw(); pin.bounce();"
 driver.execute_script(javaScript)
-time.sleep(1)
+#time.sleep(1)
 
 
 download = driver.find_element_by_id('download')
 download.click()
-print "download clicked ..."
+print "download clicked"
 
-time.sleep(20) # wait for the image to be processed
+element = WebDriverWait(driver, 20).until(
+    EC.element_to_be_clickable((By.ID, "download"))
+)
+print "downloaded"
+
 driver.quit()
 
 
