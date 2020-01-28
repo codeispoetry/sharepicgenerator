635264c0243c8ee4e94930c2748711305a644c3f
Thomas Rose <t.rose@seidemann-web.com>
2020-01-28 15:39:20 +0100

    fixed untracked files

--- code/tests/test.py
+++ code/tests/test.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import unittest, time, os,shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
 
 
 if os.path.isfile("tests/screenshot.png"):
@@ -44,16 +50,54 @@ class ChromeSearch(unittest.TestCase):
         driver.get('http://127.0.0.1/create.php')
         self.assertIn("Sharepicgenerator", driver.title)
 
-        textEl = driver.find_element_by_id('text')
-        textEl.send_keys(Keys.CONTROL, 'a')
-        textEl.send_keys( 'Hallo Welt' )
+
+        # select template
+        driver.find_element_by_id('templateopener').click()
+        driver.find_element_by_css_selector("img[src='templates/annalena.jpg']").click()
+        time.sleep(1)
+
+        select = Select(driver.find_element_by_id('sizepresets'))
+        select.select_by_visible_text('Sharepic')
+        time.sleep(1)
+
+        # Insert texts
+        driver.find_element_by_id('text').send_keys(Keys.CONTROL, 'a')
+        driver.find_element_by_id('text').send_keys( u"Hallo [Welt]\nutf8: äöüß" )
+
+        driver.find_element_by_id('textbefore').send_keys( u"Über der Linie" )
+        driver.find_element_by_id('textafter').send_keys( u"unterhalb" )
+
+        driver.find_element_by_id('textsamesize').click()
+
+        driver.find_element_by_id('pintext').send_keys( u"Ich bin der Störer" )
 
 
-        javaScript = "$('#textsize').val(300); $('#textX').val(-100); $('#textY').val(2000); text.draw(); text.bounce();"
-        driver.execute_script(javaScript)
+        # move sliders
+        driver.find_element_by_id("eyecatchersize").click()
+        elem = driver.find_element_by_id("eyecatchersize")
+        move = ActionChains(driver)
+        move.click_and_hold( elem ).move_by_offset(-40, 0).release().perform()
+
+        driver.find_element_by_id("textsize").click()
+        elem = driver.find_element_by_id("textsize")
+        move = ActionChains(driver)
+        move.click_and_hold( elem ).move_by_offset(-40, 0).release().perform()
+
+        elem = driver.find_element_by_id("svg-text")
+        move = ActionChains(driver)
+        move.click_and_hold( elem ).move_by_offset(250, -10).release().perform()
+
+
+        # logo
+        select = Select(driver.find_element_by_id('logoselect'))
+        #select.select_by_visible_text('Banana')
+        select.select_by_value('sonnenblume-big')
+
+        #javaScript = "$('#textsize').val(300); $('#textX').val(-100); $('#textY').val(2000); text.draw(); text.bounce();"
+        #driver.execute_script(javaScript)
         driver.save_screenshot("tests/screenshot.png")
 
-        print "Click Download"
+        print "click download"
         download = driver.find_element_by_id('download')
         download.click()
 
@@ -61,11 +105,13 @@ class ChromeSearch(unittest.TestCase):
             EC.element_to_be_clickable((By.ID, "download"))
         )
         #time.sleep(1)
-        print "Downloaded"
+        print "downloaded"
 
+    #@unittest.skip("no comparison")
     def test_2_compare(self):
         print "Compare"
-        stream = os.popen('compare -metric PSNR tests/artifacts/hallo-welt.jpg tests/patterns/hallo-welt.jpg tests/artifacts/diff.jpg 2>&1')
+        filename = "hallo-welt-utf-facebook-sharepic.jpg"
+        stream = os.popen('compare -metric PSNR tests/artifacts/' + filename +' tests/patterns/' + filename + ' tests/artifacts/' + filename + '_diff.jpg 2>&1')
         output = stream.read()
         self.assertEqual("inf", output)
         
