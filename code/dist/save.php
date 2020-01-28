588c8598965220bb255134c1024ccdd97e5c0495
Thomas Rose <t.rose@seidemann-web.com>
2020-01-28 17:07:55 +0100

    introduce saving to public beta

--- code/dist/save.php
+++ code/dist/save.php
@@ -6,6 +6,7 @@ if( !isset($_POST['accesstoken'] )) die();
 
 $user = preg_replace('/[^a-zA-Z0-9]/','', $_POST['user']);
 $accesstoken = preg_replace('/[^a-zA-Z0-9]/','', $_POST['accesstoken']);
+$action = preg_replace('/[^a-zA-Z0-9]/','', $_POST['action']);
 $data = $_POST['data'];
 
 $return = array();
@@ -14,10 +15,22 @@ if( !checkPermission( $user, $accesstoken )){
     die('-1');
 };
 
-if( savePic( $user, $data )){
-    $return['success'] = true;
-    die(json_encode($return));
+switch( $action ){
+    case 'save':
+        if( savePic( $user, $data )){
+            $return['success'] = true;
+            die(json_encode($return));
+        }
+        break;
+    case 'delete':
+        deleteSavedPic( $user );
+        break;
+    default:
+        $return['success'] = false;
+        die(json_encode($return));
+
 }
+
 
 
 
@@ -31,7 +44,20 @@ function checkPermission( $user, $accesstoken){
 }
 
 
+function deleteSavedPic( $user ){
+    $userDir = sprintf('persistent/user/%s', $user);
+    $userSaveFile = $userDir . '/save.txt';
 
+    $json = json_decode(file_get_contents($userSaveFile));
+    $file = $json->fullBackgroundName;
+    if( file_exists($file)){
+        unlink($file);
+    }
+
+    if( file_exists($userSaveFile)){
+        unlink($userSaveFile);
+    }
+}
 
 function savePic( $user, $data ){
 
@@ -44,10 +70,19 @@ function savePic( $user, $data ){
     $params = array();
     parse_str($data, $params);
 
-    // save uploaded image persisten
-    $new_name = $userDir .'/' . basename($params['fullBackgroundName']);
-    copy($params['fullBackgroundName'], $new_name);
-    $params['fullBackgroundName'] = $new_name;
+    // save uploaded image persistent
+    if( is_file($params['fullBackgroundName']) ) {
+        $new_name = $userDir . '/' . basename($params['fullBackgroundName']);
+        copy($params['fullBackgroundName'], $new_name);
+        $params['fullBackgroundName'] = $new_name;
+    }
+
+    // save uploaded icon persistent
+    if( is_file($params['fullBackgroundName']) ) {
+        $new_name = $userDir . '/' . basename($params['fullBackgroundName']);
+        copy($params['fullBackgroundName'], $new_name);
+        $params['fullBackgroundName'] = $new_name;
+    }
 
     file_put_contents($userSaveFile, json_encode($params));
     return true;
