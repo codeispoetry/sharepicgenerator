ad6e7c9a5e2f7ee8d991788754384519cfdcd253
Thomas Rose <t.rose@seidemann-web.com>
2020-01-30 09:17:51 +0100

    move files

--- code/dist/index.php
+++ code/dist/index.php
@@ -19,7 +19,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="/favicons/manifest.json">
     <meta name="msapplication-TileColor" content="#46962b">
     <meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png">
     <style>
