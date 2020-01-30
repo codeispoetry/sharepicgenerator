0ac9cc62e3eccedcfd7e71da3bca697ef290bb8b
Thomas Rose <t.rose@seidemann-web.com>
2020-01-30 12:26:48 +0100

    update gitignore

--- code/build/js/vintage/00init.js
+++ code/build/js/vintage/00init.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#text').val("Social Media\nwie 1980");
+    $('#text').val("Social Media\nwie 1980.");
     $('#textX').val("279");
     $('#textY').val("111");
     claim.draw();
