8881c072847712547df207758d7751605106d9db
Thomas Rose <mail@tom-rose.de>
2020-01-30 23:12:08 +0100

    fix layout

--- code/dist/index.php
+++ code/dist/index.php
@@ -89,20 +89,20 @@ header .container {
             <i class="fas fa-pen mr-2 small"></i>eigenes Sharepic erstellen
           </a>
           <a href="bayern" class="mt-2 btn btn-info btn-sm"><i class="fas fa-pen-fancy"></i> Kommunalwahl Bayern</a>
-          <a href="vintage/" class="mt-5 btn btn-dark btn-md"><i class="fab fa-pied-piper-alt"></i> Vintage-Version zum 30./40. Geburtstag</a>
+          <a href="vintage/" class="mt-5 btn btn-dark btn-sm"><i class="fab fa-pied-piper-alt"></i> Vintage-Version zum 30./40. Geburtstag</a>
          
          
             <span class="mt-5 cursor-pointer testaccess">
                 <i class="fas fa-sign-in-alt"></i> Gastzugang
             </span>
             <div class="mt-1 testaccess" style="display:none">
-                <form method="post" action="federal/" class="form-inline">
+                <form method="post" action="bayern/" class="form-inline">
                     <div class="mt-2">
                         <input type="text" class="form-control" name="pass" placeholder="Passwort eingeben">
                         <input type="submit" class="btn btn-sm btn-info" value="okay">
                     </div>
                 </form>
-                <a href="MAILTO:mail@tom-rose.de?subject=Sharepicgenerator" class="text-white">
+                <a href="MAILTO:mail@tom-rose.de?subject=Sharepicgenerator" class="d-none text-white">
                   <i class="fas fa-envelope"></i> Testzugang beantragen
                 </a>
             </divmt-3>
