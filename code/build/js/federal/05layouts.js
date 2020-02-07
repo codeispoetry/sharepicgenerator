3e8d272f3e2cd091f79c2ebfe9fb9dccda8cdd89
Thomas Rose <mail@tom-rose.de>
2020-02-07 23:47:28 +0100

    refactor layout toggler

--- code/build/js/federal/05layouts.js
+++ code/build/js/federal/05layouts.js
@@ -1,12 +1,16 @@
 function showLayout(){
-    $('[data-layout]').toggleClass("btn-info");
-    $('[data-layout]').toggleClass("btn-outline-info");
+   $('[data-layout]').removeClass("btn-info").addClass("btn-outline-info");
+
+    config.layout =  $(this).data("layout");
+    $(this).addClass("btn-info").removeClass("btn-outline-info");
 
     $(".noquote").toggleClass("d-none");
 
-    config.layout =  $('[data-layout].btn-info').data("layout");
-
     // this better with trigger
+    background.svg.unmask();
     quote.draw();
+    inverted.draw();
     text.draw();
 }
+
+$('[data-layout]').click( showLayout );
