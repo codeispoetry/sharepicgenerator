793689f6f76ff3c071957245b022740252f8c402
Thomas Rose <t.rose@seidemann-web.com>
2020-02-11 11:47:45 +0100

    update claim for bayern to edit by user

--- code/dist/bayern/cockpit.php
+++ code/dist/bayern/cockpit.php
@@ -118,7 +118,7 @@
 
         <div class="list-group-item list-group-item-action flex-column align-items-start">
             <div class="d-flex w-100 justify-content-between">
-                <h6 class="mb-1">Zeile "Am 15. März ..."</h6>
+                <input type="text" name="claim" id="claim" value="" placeholder="Am 15. März grün wählen" class="form-control">
                 <i class="fa fa-broom ml-1 text-primary cursor-pointer claim-change-color ml-1" id="claim-change-color" title="Farbe wechseln"></i>
             </div>
         </div>
