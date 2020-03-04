df09805713c81134d11eac74648947abe00e132a
Thomas Rose <t.rose@seidemann-web.com>
2020-03-04 12:09:44 +0100

    add gridlines for mosaic

--- code/build/js/08gridlines.js
+++ code/build/js/08gridlines.js
@@ -1,3 +1,15 @@
 $('#gridlines').bind('change', function(){
     $('.gridline').toggleClass('d-none');
 });
+
+function showMosaicLines(){
+    for (i = 1; i <= 2; i++) {
+        $("#canvas").append('<div class="gridline horizontal mosaicline" style="top:' + ( 100 * i / 3 ) + '%;"></div>');
+        $("#canvas").append('<div class="gridline vertical mosaicline" style="top:0;left:' + ( 100 * i / 3 ) + '%;"></div>');
+    }
+}
+
+function deleteMosaicLines(){
+    $('.mosaicline').remove();
+}
+
