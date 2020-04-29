10ad7ff531777063668f01cbe90452a18ac5c33e
Thomas Rose <t.rose@seidemann-web.com>
2020-04-29 16:18:10 +0200

    fix bayern and vintag for webpack

--- code/dist/vintage/index.php
+++ code/dist/vintage/index.php
@@ -122,7 +122,7 @@ $accessToken = createAccessToken( $user );
 <script src="../vendor/svg.min.js"></script>
 <script src="../vendor/svg.draggable.min.js"></script>
 <script src="../vendor/svg.filter.min.js"></script>
-<script src="../assets/js/main.min.js"></script>
+<script src="main-allgemein.min.js"></script>
 <script src="main.min.js"></script>
 
 </body>
