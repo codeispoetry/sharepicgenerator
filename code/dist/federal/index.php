8107c0da9b0bd736f60903e5277b187f421e1d2d
Thomas Rose <t.rose@seidemann-web.com>
2020-05-05 14:51:35 +0200

    add link to documentation

--- code/dist/federal/index.php
+++ code/dist/federal/index.php
@@ -119,12 +119,13 @@ $accessToken = createAccessToken( $user );
 
 <footer class="row bg-primary p-2 text-white">
     <div class="col-12 col-lg-6">
-        <a href="https://github.com/codeispoetry/sharepicgenerator" target="_blank">Quellcode auf github.com</a> |
-        <a href="/imprint.php">Impressum</a>
+        <a href="/documentation" target="_blank"><i class="fas fa-question-circle"></i> Anleitung</a>
     </div>
 
     <div class="col-12 col-lg-6 text-lg-right">
         <a href="https://chatbegruenung.de/channel/sharepicgenerator" target="_blank">Feedback im Chat-Channel</a> |
+        <a href="https://github.com/codeispoetry/sharepicgenerator" target="_blank">Quellcode auf github.com</a> |
+        <a href="/imprint.php" target="_blank">Impressum</a> |
         Programmiert mit <i class="fas fa-heart text-yellow"></i> von 
         <a href="MAILTO:mail@tom-rose.de?subject=Sharepicgenerator">Tom Rose</a>.
     </div>
