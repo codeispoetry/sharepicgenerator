98173fb90c7261df88a54d637d889b59c5f29e7e
Thomas Rose <t.rose@seidemann-web.com>
2020-05-14 13:41:16 +0200

    improve test, fail

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,18 @@
-const core = require('@actions/core');
+var auth = require('./auth.json');
+
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
         await driver.get('https://sharepicgenerator.de');
-        //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
         await driver.wait(until.titleIs('Sharepicgenerator'), 1000);
+        await driver.findElement(By.id('test-access-password')).sendKeys(auth.password, Key.RETURN);
+        await driver.findElement(By.id('download')).click();
         console.log("passed");
     } catch( error ){
+        const core = require('@actions/core');
         core.setFailed(error.message);
     } finally {
         await driver.quit();
