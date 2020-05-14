042cc9f04ceb38044eed5be7c902ac0b277ecfb2
Thomas Rose <t.rose@seidemann-web.com>
2020-05-14 12:02:39 +0200

    add test

---
+++ tests/test.js
@@ -0,0 +1,20 @@
+/*
+run locally with
+SELENIUM_REMOTE_URL="http://localhost:4444/wd/hub" node test.js
+ */
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('https://sharepicgenerator.de');
+        //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('Sharepicgenerator'), 1000);
+        console.log("passed");
+    } catch(e){
+        console.error("Failure");
+    } finally {
+        await driver.quit();
+    }
+})();
