292e578e99a5f26b8cd4e32b8caaa7a3bafd76ef
Thomas Rose <t.rose@seidemann-web.com>
2020-05-14 12:56:51 +0200

    test should pass

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     try {
         await driver.get('https://sharepicgenerator.de');
         //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        await driver.wait(until.titleIs('Sharepicgeneratord'), 1000);
+        await driver.wait(until.titleIs('Sharepicgenerator'), 1000);
         console.log("passed");
     } catch( error ){
         core.setFailed(error.message);
