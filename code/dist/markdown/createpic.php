0833f69d1666910603c0c5df9ca17e732ffdd801
Thomas Rose <t.rose@seidemann-web.com>
2020-05-15 15:23:24 +0200

    add markdown generator

---
+++ code/dist/markdown/createpic.php
@@ -0,0 +1,23 @@
+<?php
+
+$uniqid = uniqid('md2html');
+//$uniqid = "1";
+$filename = 'tmp/' . $uniqid . '.html';
+$filenamePic = 'tmp/' . $uniqid . '.png';
+
+$html = $_POST['html'];
+$width = (int) $_POST['width'];
+$height = (int) $_POST['height'];
+
+file_put_contents($filename, sprintf("<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"../styles.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"../../assets/css/styles.css\"></head><body>%s</body></html>",$html));
+
+$command = sprintf("chromium --headless --screenshot=%s --disable-gpu -default-background-color=0 --no-sandbox --window-size=%d,%d %s 2>&1", $filenamePic, $width, $height, $filename);
+exec( $command, $output );
+
+$command = sprintf("chmod 755 %s", $filenamePic);
+exec( $command, $output );
+
+$return = [];
+$return['basename'] = basename($filenamePic, 'png');
+$return['error'] = $output;
+echo json_encode($return);
