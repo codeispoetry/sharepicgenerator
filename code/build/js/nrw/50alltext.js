aeca72f549760dae3f6390beec295f4be6d99062
Thomas Rose <t.rose@seidemann-web.com>
2020-07-06 10:51:03 +0200

    beautify nrw cockpit

--- code/build/js/nrw/50alltext.js
+++ code/build/js/nrw/50alltext.js
@@ -2,12 +2,16 @@ var textColors = ["white","black","#46962b","#E6007E","#FEEE00"];
 
 
 function textChangeColor(){
-    let textColorIndex = parseInt( $('#textColor').val() );
+    let id = $(this).data('id');
+
+    let textColorIndex = parseInt( $('#textColor' + id).val() );
     textColorIndex++;
     textColorIndex %= textColors.length;
-    
-    $('#textColor').val( textColorIndex );
+
+
+    $('#textColor' + id ).val( textColorIndex );
 
     text.draw();
-    quote.draw();
 }
+
+$('.text-change-color').click( textChangeColor );
