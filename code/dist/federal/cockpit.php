9584cab69f0d78de34b06250be4a102ffe338554
Thomas Rose <t.rose@seidemann-web.com>
2020-07-09 13:37:50 +0200

    wip: add saving own cloudcredentials

--- code/dist/federal/cockpit.php
+++ code/dist/federal/cockpit.php
@@ -300,21 +300,38 @@
 
         <h3 class="collapsed" data-toggle="collapse" data-target=".cloud">Wolke</h3>
         <div class="cloud KILLcollapse list-group-item list-group-item-action flex-column align-items-start">
-            <div>
+            <?php
+                if( hasCloudCredentials() ) {
+                    echo '<script type="text/javascript">config.hasCloudCredentials = true;</script>';
+                }
+            ?>
+
+            <div class="d-flex justify-content-between">
                 <a href="https://wolke.netzbegruenung.de/apps/files/?dir=/sharepicgenerator" target="_blank">
-                    <i class="fas fa-link"></i> Meine Wolke
+                    <i class="fas fa-cloud"></i> Meine Wolke
+                </a>
+                <a href="" class="cloudtokendelete">
+                    <i class="fas fa-ban"></i> Zugang zur Wolke trennen
                 </a>
             </div>
-            <div id="cloudnotoken" class="">
-                Bitte hinterlege Deine Zugangsdaten zur Wolke.
+            <div id="cloudmessage" style="display:none">
+                <p class="bg-danger p-1 pl-3 text-white">Verbinde mich mit Wolke ...</p>
+            </div>
+            <div id="cloudnotoken" class="" style="display:none">
+                 <div>
+                     <a href="https://wolke.netzbegruenung.de/settings/user/security" target="_blank">
+                         Neues App-Passwort erstellen
+                     </a> |
+                    <a href="/documentation/cloud" target="_blank">
+                        Anleitung
+                    </a>
+                 </div>
+                Bitte hinterlege Deine App-Passwort aus der Wolke:
                 <input type="text"  class="form-control" name="cloudtoken" id="cloudtoken">
                 <input type="button" id="cloudtokensave" class="btn btn-sm btn-primary" value="Token speichern">
             </div>
             <div id="cloudhastoken" style="display: none">
                 <div>
-                    <div id="cloudmessage" style="display:none">
-                        <p class="bg-danger p-1 text-white"></p>
-                    </div>
                     <select class="form-control" id="cloudfiles" disabled>
                         <option value="">lade Sharepics ...</option>
                     </select>
