306746f1ea54dc2fb3eb41631a1ff8ab42e6d14f
Thomas Rose <t.rose@seidemann-web.com>
2020-08-22 12:28:17 +0200

    fix position of eyecatcher in nrw

--- code/build/js/nrw/75eyecatcher.js
+++ code/build/js/nrw/75eyecatcher.js
@@ -18,7 +18,8 @@ const pin = {
   draw() {
     if (!pin.isLoaded) return false;
 
-    const offsetLeft = -154 / 948;
+    const offsetLeft = -45 / 948;
+    console.log("w",offsetLeft);
     const pinMoveX = offsetLeft * pin.svg.width();
     const pinMoveY = draw.height() - pin.svg.height() - 70;
     pin.svg.size(draw.width() * 0.33).move(pinMoveX, pinMoveY);
