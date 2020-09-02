3b05b993c86cfd5744d7d1248aa9e2594f3da5c8
Thomas Rose <t.rose@seidemann-web.com>
2020-09-02 15:34:03 +0200

    add console.log to python test

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,4 @@
-import unittest
-import os
+import unittest, time, os,shutil
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -7,9 +6,17 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 class sharepicgenerator(unittest.TestCase):
 
     def setUp(self):
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument('--window-size=1280,800')
+        self.chrome_options.add_argument('--ignore-certificate-errors')
+        self.chrome_options.add_argument("--incognito")
+
         self.driver = webdriver.Remote(
-                          "http://localhost:4444/wd/hub",
-                          desired_capabilities=DesiredCapabilities.CHROME)
+                   command_executor='http://localhost:4444/wd/hub',
+                   desired_capabilities=DesiredCapabilities.CHROME,
+                   options=self.chrome_options)
+
+        self.driver.delete_all_cookies()
         self.driver.implicitly_wait(10) # seconds
 
 
@@ -29,6 +36,10 @@ class sharepicgenerator(unittest.TestCase):
         #elem.send_keys(Keys.RETURN)
         # assert "No results found." not in driver.page_source
 
+        driver.save_screenshot("screenshot.png")
+
+        for entry in driver.get_log('browser'):
+            print(entry)
 
     def tearDown(self):
         self.driver.quit()
