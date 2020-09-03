513870491680bd7a05ebe11cfcf018ff21090802
Thomas Rose <t.rose@seidemann-web.com>
2020-09-03 12:03:12 +0200

    add password entering

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-import unittest, time, os,shutil
+import unittest, time, os, shutil, json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -17,31 +17,30 @@ class sharepicgenerator(unittest.TestCase):
                    options=self.chrome_options)
 
         self.driver.delete_all_cookies()
-        self.driver.implicitly_wait(10) # seconds
+        self.driver.implicitly_wait(30) # seconds
 
 
-    def test_startpage(self):
+    def test(self):
         driver = self.driver
         if "URL" in os.environ:
             url = os.environ['URL']
         else:
             url = "http://webserver"
 
+        # Startpage
         driver.get( url )
-
         self.assertIn("Sharepicgenerator", driver.title)
 
-        #elem = driver.find_element_by_name("q")
-        #elem.send_keys("pycon")
-        #elem.send_keys(Keys.RETURN)
-        # assert "No results found." not in driver.page_source
+        # Login
+        with open('auth.json') as auth_json:
+            auth = json.load(auth_json)
+        driver.find_element_by_id("test-access-password").send_keys(auth['password'] + u'\ue007')
 
-        driver.save_screenshot("artifacts/screenshot.png")
-
-        for entry in driver.get_log('browser'):
-            print(entry)
 
     def tearDown(self):
+        self.driver.save_screenshot("artifacts/screenshot.png")
+        for entry in self.driver.get_log('browser'):
+            print(entry)
         self.driver.quit()
 
 if __name__ == "__main__":
