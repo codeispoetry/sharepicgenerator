6d8ac88a46f7b26c376867cf089986ca35d8a2e7
Tom Rose <mail@tom-rose.de>
2020-10-07 13:49:52 +0200

    wip: make eraser smoother

--- code/build/js/80eraser.js
+++ code/build/js/80eraser.js
@@ -1,6 +1,9 @@
 const eraser = {
   svg: draw.circle(0),
   backgroundClone: draw.circle(0),
+  active: false,
+  pointSize: 15,
+  pointer: draw.circle(0).fill('black').opacity(0.5),
 
   draw() {
     eraser.svg.remove();
@@ -30,16 +33,46 @@ const eraser = {
     eraser.backgroundClone.clipWith(eraser.svg);
   },
 
+  drawAdd(x, y) {
+    const v = $('#eraser').val();
+    $('#eraser').val(`${v}${x},${y},${eraser.pointSize}|`);
+
+    const circle = draw.circle(eraser.pointSize).move(x, y);
+    eraser.svg.add(circle);
+  },
+
   start() {
     config.noTextDradAndDrop = true;
     $('#btn-eraser').html('Radierer ausschalten');
     $('#btn-eraser').data('action', 'off');
+    eraser.pointer.radius(eraser.pointSize / 2);
+    eraser.draw();
+    $('#canvas').css('cursor', 'none');
 
-    draw.click((e) => {
-      const v = $('#eraser').val();
-      const pointSize = 15;
-      $('#eraser').val(`${v}${e.layerX},${e.layerY},${pointSize}|`);
-      eraser.draw();
+    draw.mousedown((e) => {
+      eraser.active = true;
+      const x = e.layerX;
+      const y = e.layerY;
+      eraser.drawAdd(x, y);
+    });
+
+    draw.mouseup(() => {
+      eraser.active = false;
+    });
+
+    draw.mousemove((e) => {
+      const x = e.layerX;
+      const y = e.layerY;
+
+      eraser.pointer.move(x, y).front();
+
+      if (!eraser.active) {
+        return false;
+      }
+
+      eraser.drawAdd(x, y);
+
+      return true;
     });
   },
 
@@ -47,8 +80,9 @@ const eraser = {
     config.noTextDradAndDrop = false;
     $('#btn-eraser').html('Radierer einschalten');
     $('#btn-eraser').data('action', 'on');
+    $('#canvas').css('cursor', 'auto');
 
-    draw.off('click');
+    draw.off();
   },
 
 };
