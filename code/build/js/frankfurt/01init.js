bdbff095a4db7edf62c1b40b73b1053e159a83a9
Tom Rose <mail@tom-rose.de>
2020-10-20 14:17:45 +0200

    introduce tenant frankfurt

---
+++ code/build/js/frankfurt/01init.js
@@ -0,0 +1,62 @@
+// eslint-disable-next-line no-unused-vars
+const bgpic = {
+  width: 800,
+  height: 450,
+  originalWidth: 1920,
+  originalHeight: 1080,
+  filename: '/assets/bg_small.jpg',
+  fullBackgroundName: '../assets/bg.jpg',
+};
+
+$(document).ready(() => {
+  $('#text').val('Hier ist Frankfurt');
+  $('#textsize').val(502);
+  $('#textX').val(41);
+  $('#textY').val(172);
+  config.layout = 'nolines';
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const clickId = urlParams.get('clickId');
+  $(`#${clickId}`).click();
+
+  $('.toast').toast({ delay: 10000 });
+  $('.toast-actionday').toast('show');
+
+  $('#addtextX').val(50);
+  $('#addtextY').val(draw.height() - 50);
+
+  showLayout();
+});
+
+// eslint-disable-next-line no-unused-vars
+function reset() {
+  // do nothing, stay here
+}
+
+// eslint-disable-next-line no-unused-vars
+function reDraw(withAddPic = false) {
+  if (withAddPic === true) {
+    addPic1.draw();
+    addPic2.draw();
+  }
+
+  window.setTimeout(() => {
+    pin.draw();
+  }, 10);
+
+  window.setTimeout(() => {
+    copyright.draw();
+    icon.load();
+  }, 20);
+
+  window.setTimeout(() => {
+    logo.load();
+    text.draw();
+    addtext.draw();
+    quote.draw();
+    nolines.draw();
+    invers.draw();
+    eraser.draw();
+  }, 100);
+}
---
+++ code/build/js/frankfurt/80icon.js
@@ -0,0 +1,84 @@
+const icon = {
+  isLoaded: false,
+
+  load() {
+    if (this.svg) this.svg.remove();
+    icon.isLoaded = false;
+
+    const file = $('#iconfile').val();
+    this.svg = draw.image(file, () => {
+      icon.isLoaded = true;
+      icon.svg.size(1).move(-100, -100); // cannot be resized to zero
+      text.draw();
+      $('.iconsizeselectwrapper').removeClass('d-none');
+    });
+
+    this.svg.on('error', (e) => {
+      console.log(file, e);
+    });
+  },
+
+  remove() {
+    if (this.svg) this.svg.remove();
+    this.isLoaded = false;
+  },
+};
+
+$('#iconsize').on('change', function changeIcon() {
+  if ($(this).val() === 0) {
+    icon.remove();
+  }
+  text.draw();
+});
+
+$('#iconoverlay form').submit(() => {
+  getIcons($('#iconoverlay .q').val());
+  return false;
+});
+
+function getIcons(q) {
+  const url = `/nounproject/load_results.php?q=${q}`;
+  $('#iconoverlay .results').html('suche Icons ');
+  const loading = window.setInterval(() => {
+    $('#iconoverlay .results').append(' . ');
+  }, 10);
+  $.ajax({
+    url,
+    success(data) {
+      $('#iconoverlay .results').html('');
+      const json = JSON.parse(data);
+
+      json.hits.forEach((thisIcon) => {
+        $('#iconoverlay .results').append(`<div class="chooseicon" data-icon-url="${thisIcon.icon_url}"  data-attribution="${thisIcon.attribution}"><img src="${thisIcon.preview_url}" title="${thisIcon.attribution}"/></div>`);
+      });
+
+      if (json.hits.length === 0) {
+        $('#iconoverlay .results').append('<div class="col-12 bg-danger text-white p-3 text-center">Keine Icons gefunden. Bitte suche auf Englisch.</div>');
+      }
+
+      $('#iconoverlay .results .chooseicon').click(function clickChoose() {
+        $('#waiting').show();
+        $('#iconoverlay').hide();
+
+        const nounprojectattribution = $(this).data('attribution');
+
+        $.get('/nounproject/get_icon.php', { icon_url: $(this).data('icon-url') })
+          .done((iconData) => {
+            if (iconData === 'error') {
+              console.log('error downloading icon');
+            } else {
+              $('#iconfile').val(`/tmp/${iconData}`);
+              icon.load();
+              setCopyright(nounprojectattribution, 'nounproject');
+            }
+            closeOverlay();
+          });
+      });
+
+      window.clearInterval(loading);
+    },
+    error(data, textStatus, jqXHR) {
+      console.log(data, jqXHR);
+    },
+  });
+}
