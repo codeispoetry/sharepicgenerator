c9d457541e7405f4f5587e95955ca91494be565a
Tom Rose <mail@tom-rose.de>
2020-10-26 15:54:08 +0100

    disable cache for logs

--- code/dist/log/index.php
+++ code/dist/log/index.php
@@ -11,6 +11,9 @@ require_once(getBasePath('lib/log_functions.php'));
     <meta name="theme-color" content="#46962b">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Logs</title>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
     <style>
         .graphCanvas{
