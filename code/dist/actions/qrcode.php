3da5563fd25fb068afbe27a6dbf06bd1b4b8b364
Tom Rose <mail@tom-rose.de>
2020-11-07 19:34:03 +0100

    log qrcode download

--- code/dist/actions/qrcode.php
+++ code/dist/actions/qrcode.php
@@ -3,6 +3,18 @@ require_once('base.php');
 require_once(getBasePath('lib/functions.php'));
 $file = sprintf('%s/%s.jpg', getBasePath('tmp'), sanitizeUserinput($_GET['f']));
 
+if (isAdmin()) {
+    $db->exec('CREATE TABLE IF NOT EXISTS qrcode(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            url TEXT)');
+}
+
+$smt = $db->prepare('INSERT INTO qrcode (url) values (:file)');
+$smt->bindValue(':url', $file, SQLITE3_TEXT);
+$smt->execute();
+
+
 header('Content-Type: image/jpg');
 header("Content-Length: ".filesize($file));
 header('Content-Disposition: attachment; filename="sharepic.jpg"');
