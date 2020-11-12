e82002f072ff57570769f28965f2dc2af96c219a
Tom Rose <mail@tom-rose.de>
2020-11-12 10:08:58 +0100

    add link to corporate design

--- code/dist/tenants/federal/index.php
+++ code/dist/tenants/federal/index.php
@@ -98,6 +98,9 @@ nextActionDay();
                     <a href="#" class="overlay-opener dropdown-item" data-target="actiondays" id="actiondaysopener">
                         <i class="far fa-hand-point-right"></i> Aktionstage
                     </a>
+                    <a href="https://www.gruene.de/service/corporate-design" class="dropdown-item" target="_blank">
+                        <i class="fas fa-tape"></i> Designrichtlinien
+                    </a>
                 </div>
             </li>
             <li class="nav-item dropdown">
