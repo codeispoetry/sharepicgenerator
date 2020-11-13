b968423a65b5ec9ff2d293547fb04cf4403ae298
Tom Rose <mail@tom-rose.de>
2020-11-13 18:08:17 +0100

    alert when uploaded file to big

--- code/dist/lib/toasts/toasts.php
+++ code/dist/lib/toasts/toasts.php
@@ -34,6 +34,37 @@
         Um eine eigene Vorlage zu veröffentlichen, klicke rechts auf <em>Vorlagen</em>.
       </div>
     </div>
+
+    <div class="toast upload-too-big" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header bg-info text-white">
+
+        <strong class="mr-auto">Bild sehr groß</strong>
+        <small class="small text-white"></small>
+        <button type="button" class="ml-2 mb-1 close text-danger text-shadow-none" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        Dein Bild ist ziemlich groß. Der Sharepicgenerator braucht dafür recht lange.<br>
+        Schneller geht's mit Bildern, die kleiner als 4000 Pixel sind.
+      </div>
+    </div>
+
+    <div class="toast face-alert" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header bg-info text-white">
+
+        <strong class="mr-auto">Gesicht</strong>
+        <small class="small text-white"></small>
+        <button type="button" class="ml-2 mb-1 close text-danger text-shadow-none" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        Das Bild zeigt ein Gesicht. Du brauchst die Erlaubnis der abgebildeten Person, um das Foto zu verwenden.
+      </div>
+    </div>
+
+
     <?php
         echo $toasts;
     ?>    
