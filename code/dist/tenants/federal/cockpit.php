feef50c23944f8457c5e9a2ae30ee10381b552c3
Tom Rose <mail@tom-rose.de>
2020-12-13 15:57:48 +0100

    move helpgrid to cockpit

--- code/dist/tenants/federal/cockpit.php
+++ code/dist/tenants/federal/cockpit.php
@@ -514,6 +514,19 @@ if (!isAllowed(false)) {
                 <a href="/documentation/code" target="_blank"><i class="fas fa-book"></i> Anleitung</i></a>
             </div>
         </div>
+
+        <div class="text-center text-white mt-3 border-bottom pb-3">
+            <div class="d-none d-md-block col-12">
+                <span class="small mr-2">Hilflinien:</span>
+                <input id="gridlines" type="checkbox" checked 
+                    data-width="40" data-size="xs" data-toggle="toggle" data-on="an" data-off="aus">
+                
+                <span class="small ml-4 mr-2">Mehr Funktionen:</span>
+                <input id="expertmode" type="checkbox" 
+                    data-width="40" data-size="xs" data-toggle="toggle" data-on="an" data-off="aus">
+            </div>
+        </div>    
+
         <div class="text-center text-white mt-3">
             <i class="fas fa-spa text-highlight"></i> Programmiert von
             <a href="MAILTO:mail@tom-rose.de?subject=Sharepicgenerator" class="text-white">Tom Rose</a>.
