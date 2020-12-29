c65979b72e5ffd664c455ec1682899c5620a9a2d
Tom Rose <mail@tom-rose.de>
2020-12-29 21:00:25 +0100

    add faq

---
+++ code/dist/lib/overlays/faq.php
@@ -0,0 +1,30 @@
+<div class="col-10 offset-2">
+    <div id="faq" class="overlay" style="display:none;">
+        <a href="#" class="close closer text-danger" data-target="#faq">
+            <i class="fas fa-times"></i>
+        </a>
+        <h2>Häufige Fragen</h2>
+    
+        <div class="row">
+            <div class="col-8">
+                <h5>Wer darf den Sharepicgenerator nutzen?</h5>
+                <p>Jedes Mitglied der Grünen darf den Sharepicgenerator im Sinne unseres Grundsatzprogrammes nutzen.</p>
+
+                <h5>Kann ich auch ein eigenes Logo, z.B. der grünen Jugend hochladen?</h5>
+                <p>Ja, unter <em>Logo</em> kannst Du beliebig viele eigene Logos hochladen.</p>
+
+                <h5>Zur Landtagswahl haben wir eine eigene Kampagne. Kann ich dafür auch Sharepic erstellen?</h5>
+                <p>Es gibt regionale Angebote für einzelne Bundesländer und Kreisverbände. Diese findest Du oben im Menü.</p>
+
+                <h5>Kann ich die Sharepics auch ausdrucken?</h5>
+                <p>Ja. Unter <em>Größe</em> kannst Du eine Vorlage, z.B. für eine Postkarte auswählen oder die Größe beliebig einstellen.</p>
+
+                <h5>Ich habe kein passendes Hintergrundbild. Wo bekomme ich Bilder her?</h5>
+                <p>
+                    Bei <em>Hauptbild</em> gibt es eine Suchmaske. Aktuell werden drei verschiedene Bilddatenbanken durchsucht. 
+                    Deren Bilder darfst Du kostenlos nutzen.
+                </p>
+            </div>
+        </div> 
+    </div>
+</div>
