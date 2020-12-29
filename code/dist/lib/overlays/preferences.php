7ce47ec870f7ec8c0013e1d5b042d4600b6180dd
Tom Rose <mail@tom-rose.de>
2020-12-29 11:25:18 +0100

    synchronize menu

---
+++ code/dist/lib/overlays/preferences.php
@@ -0,0 +1,8 @@
+<div class="col-12">
+    <div id="preferences" class="overlay" style="display:none;">
+        <a href="#" class="close closer text-danger" data-target="#preferences">
+            <i class="fas fa-times"></i>
+        </a>
+        <h2>Einstellungen</h2>
+    </div>
+</div>
