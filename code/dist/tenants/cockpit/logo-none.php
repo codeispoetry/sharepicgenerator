ef6274efae82470b8e3c74eba21a2b94ad5f9108
Tom Rose <mail@tom-rose.de>
2021-01-09 22:46:15 +0100

    rlp refactor cockpit

---
+++ code/dist/tenants/cockpit/logo-none.php
@@ -0,0 +1,44 @@
+<h3 class="d-none collapsed" data-toggle="collapse" data-target=".logo"><i class="fas fa-fan"></i> Logo</h3>
+<div class="d-none logo collapse list-group-item list-group-item-action flex-column align-items-start">
+    <div class="mb-1 d-flex align-items-lg-center">
+        <select class="form-control" name="logoselect" id="logoselect">
+            <optgroup label="Sonnenblume">
+                <option value="sonnenblume">Sonnenblume</option>
+                <option value="sonnenblume-weiss">weiße Sonnenblume</option>
+                <option value="sonnenblume-big">Sonnenblume links unten</option>
+            </optgroup>
+            <optgroup label="Logo mit Schriftzug">
+                <option value="logo-weiss">weiß, mit Schriftzug</option>
+                <option value="logo-gruen">grün, mit Schriftzug</option>
+            </optgroup>        
+            <optgroup label="Speziallogos">
+                <option value="frauenrechte">Frauenrechte</option>
+                <option value="regenbogen">Regenbogen</option>
+                <option value="europa">Europa</option>
+            </optgroup>
+            <optgroup label="Kein Logo">
+                <option value="void">kein Logo</option>
+            </optgroup>
+        </select>
+            <i class="fa fa-upload text-primary cursor-pointer uploadlogoclicker ml-2" title="Eigenes Logo hochladen"></i>
+            <i class="fa fa-trash text-primary cursor-pointer overlay-opener nav-lin ml-2" data-target="preferences" title="Logos löschen"></i>
+
+    </div>
+    <div class="">
+        <input type="text" placeholder="Text im blauen Balken: KV oder OV" name="logochapter" id="logochapter" value=""
+                class="form-control form-control-sm">
+    </div>
+    <div class="d-flex justify-content-between">
+        <div class="slider">
+            <small>klein</small>
+                <input type="range" class="custom-range" name="logosize" id="logosize" min="1" max="100" value="10">
+            <small>groß</small>
+        </div>
+        <div>
+            <span class="to-front" data-target="logo" title="Logo nach vorne">
+                <i class="fas fa-layer-group text-primary"></i>
+            </span> 
+        </div>
+    </div>
+</div>
+<input type="file" class="custom-file-input upload-file" id="uploadlogo" accept="image/*">
---
+++ code/dist/tenants/cockpit/rlp/eyecatcher.php
@@ -0,0 +1,26 @@
+<h3 class="d-none collapsed" data-toggle="collapse" data-target=".eyecatcher"><i class="far fa-eye"></i> Störer</h3>
+<div class="d-none eyecatcher list-group-item list-group-item-action flex-column align-items-start collapse">
+    <div class="mb-1 list-group-item-content">
+        <div class="d-flex align-items-lg-center">
+            <textarea name="pintext" id="pintext" placeholder="Störertext. Maximal 2 Zeilen." class="form-control">Zweitstimme
+grün
+            </textarea>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="slider">
+                <small>klein</small>
+                <input type="range" class="custom-range" name="eyecatchersize" id="eyecatchersize" min="50"
+                    max="300" value="100" disabled>
+                <small>groß</small>
+            </div>
+            <div>
+                <span class="to-front" data-target="pin" title="Störer nach vorne">
+                    <i class="fas fa-layer-group text-primary"></i>
+                </span> 
+            </div>
+        </div>    
+    </div>
+</div>
+
+<input type="hidden" name="pinX" id="pinX">
+<input type="hidden" name="pinY" id="pinY">
