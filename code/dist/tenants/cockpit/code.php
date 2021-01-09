f4aad91c59a6dbf9790e42e4fcf264b0f8fe8aca
Tom Rose <mail@tom-rose.de>
2021-01-09 22:15:46 +0100

    refactor cockpit federal

---
+++ code/dist/tenants/cockpit/code.php
@@ -0,0 +1,10 @@
+<h3 class="collapsed d-none" data-toggle="collapse" data-target=".code"><i class="fas fa-code"></i> Code-API</h3>
+        <div class="code collapse list-group-item list-group-item-action flex-column align-items-start">
+            <div>
+                <textarea placeholder="JavaScript-Code" name="code" id="code" class="form-control"></textarea>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <button type="button" class="btn btn-info btn-sm runcode"><i class="fas fa-code"></i> Code ausführen</button>
+                <a href="/documentation/code" target="_blank"><i class="fas fa-book"></i> Anleitung</i></a>
+            </div>
+        </div>
---
+++ code/dist/tenants/cockpit/eraser.php
@@ -0,0 +1,19 @@
+
+
+        <h3 class="collapsed expertmode showonly nolines quote lines" data-toggle="collapse" data-target=".eraser"><i class="fas fa-eraser"></i> 
+            3-D-Effekt
+         </h3>
+        <div class="eraser expertmode collapse list-group-item list-group-item-action flex-column align-items-start">
+           Um eine 3-D-Anmtung zu bekommen, kannst Du Text wegradieren. Dadurch entseht der Eindruck, der 
+           Text stünde hinter einem Objekt.
+           
+            <div class="d-flex justify-content-between mb-2">
+                <a class="btn btn-info btn-sm" id="btn-eraser" data-action="on">Radierer einschalten</a>
+                <span id="eraser-delete">
+                    <i class="fas fa-trash text-primary" title="löschen"></i>
+                </span>
+            </div>
+            <img src="/assets/3d-effekt.jpg" class="img-fluid">
+        </div>
+
+        <input type="hidden" name="eraser" id="eraser">
