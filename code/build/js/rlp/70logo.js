fe4d5b8d49b4eb51181dd2d56cc2a5f1b30236bb
Tom Rose <mail@tom-rose.de>
2021-01-10 10:20:12 +0100

    rlp: add logo

--- code/build/js/rlp/70logo.js
+++ code/build/js/rlp/70logo.js
@@ -9,55 +9,13 @@ const logo = {
   isLoaded: false,
   config: {
     sonnenblume: {
-      file: '/assets/logos/sonnenblume.svg',
-      widthFraction: 0.1,
-      position: 'topright',
+      file: '/assets/logos/rlp/logo.svg',
+      widthFraction: 0.3,
+      position: 'bottomright',
     },
     'sonnenblume-weiss': {
       file: '/assets/logos/sonnenblume-weiss.svg',
       widthFraction: 0.1,
-      position: 'topright',
-    },
-    frauenrechte: {
-      file: '/assets/logos/frauenrechte.svg',
-      widthFraction: 0.1,
-      position: 'topright',
-    },
-    europa: {
-      file: '/assets/logos/europa.svg',
-      widthFraction: 0.15,
-      position: 'topright',
-    },
-    regenbogen: {
-      file: '/assets/logos/regenbogen.png',
-      widthFraction: 0.1,
-      position: 'topright',
-    },
-    'logo-weiss': {
-      file: '/assets/logos/logo-weiss.svg',
-      widthFraction: 0.2,
-      position: 'topright',
-      showChapter: true,
-    },
-    'logo-gruen': {
-      file: '/assets/logos/logo-gruen.svg',
-      widthFraction: 0.20,
-      position: 'topright',
-      showChapter: true,
-    },
-    'sonnenblume-big': {
-      file: '/assets/logos/sonnenblume-viertel.svg',
-      widthFraction: 0.28,
-      position: 'bottomleftbig',
-    },
-    'logo-berlin-weiss': {
-      file: '/assets/logos/berlin-weiss.svg',
-      widthFraction: 0.2,
-      position: 'topright',
-    },
-    'logo-berlin-gruen': {
-      file: '/assets/logos/berlin-gruen.svg',
-      widthFraction: 0.2,
       position: 'topright',
     },
     custom: {
@@ -107,6 +65,10 @@ const logo = {
     switch (logo.logoinfo.position) {
       case 'bottomleft':
         x = 10;
+        y = draw.height() - logo.svg.height() - 10 - 20;
+        break;
+      case 'bottomright':
+        x = draw.width() - width - 10;
         y = draw.height() - logo.svg.height() - 10 - 20;
         break;
       case 'bottomleftbig':
