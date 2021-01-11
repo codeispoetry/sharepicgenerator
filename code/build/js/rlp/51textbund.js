d1dfbfddf2d5fae59fd63fc2259f5c9b1adea51c
Tom Rose <mail@tom-rose.de>
2021-01-11 10:18:41 +0100

    add color behind text

--- code/build/js/rlp/51textbund.js
+++ code/build/js/rlp/51textbund.js
@@ -170,7 +170,7 @@ const text = {
     text.grayBackground.remove();
     if ($('#graybehindtext').prop('checked')) {
       const grayGradient = draw.gradient('radial', (add) => {
-        add.stop({ offset: 0, color: '#000', opacity: 0.9 });
+        add.stop({ offset: 0, color: $('#colorbehindtext').val(), opacity: 0.9 });
         add.stop({ offset: 0.9, color: '#000', opacity: 0.0 });
       });
       grayGradient.from(0.5, 0.5).to(0.5, 0.5).radius(0.5);
