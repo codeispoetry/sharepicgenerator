a2b73609246f4cd66feaf5fdf6d6cd2cc4dad7ff
Tom Rose <mail@tom-rose.de>
2021-01-18 20:31:40 +0100

    add mail to tenants

--- code/dist/tenants/frankfurt/cockpit.php
+++ code/dist/tenants/frankfurt/cockpit.php
@@ -392,28 +392,7 @@
                 </div>    
             </div>
         </div>
-        <h3 class="collapsed d-none" data-toggle="collapse" data-target=".addtext"><i class="fa fa-asterisk"></i> Sternchentext</h3>
-        <div class="addtext list-group-item list-group-item-action flex-column align-items-start collapse">
-            <div class="mb-1 list-group-item-content">
-                <div class="d-flex align-items-lg-center">
-                    <textarea name="pintext" id="addtext" placeholder="Sternchentext" value="" class="form-control"></textarea>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <div class="slider">
-                        <small>klein</small>
-                        <input type="range" class="custom-range" name="addtextsize" id="addtextsize" min="0"
-                            max="50" value="20">
-                        <small>groß</small>
-                    </div>
-                    <div>
-                        <span class="to-front" data-target="addtext" title="Sternchentext nach vorne">
-                            <i class="fas fa-layer-group text-primary"></i>
-                        </span> 
-                        <i class="fa fa-broom ml-1 text-primary cursor-pointer addtext-change-color ml-1" title="Farbe wechseln"></i>
-                    </div>
-                </div>    
-            </div>
-        </div>
+        <?php require_once(getBasePath('tenants/cockpit/addtext.php')); ?>
 
         <h3 class="collapsed showonly nolines quote lines" data-toggle="collapse" data-target=".eraser"><i class="fas fa-eraser"></i> 
             3-D-Effekt
@@ -546,6 +525,9 @@
             </div>
         </div>
 
+        <?php require_once(getBasePath('tenants/cockpit/mail.php')); ?>
+
+
         <h3 class="collapsed d-none" data-toggle="collapse" data-target=".code"><i class="fas fa-code"></i> Code-API</h3>
         <div class="code collapse list-group-item list-group-item-action flex-column align-items-start">
             <div>
@@ -580,8 +562,7 @@
         <input type="hidden" name="fullBackgroundName" id="fullBackgroundName">
         <input type="hidden" name="textX" id="textX">
         <input type="hidden" name="textY" id="textY">
-        <input type="hidden" name="addtextX" id="addtextX">
-        <input type="hidden" name="addtextY" id="addtextY">
+       
         <input type="hidden" name="addPic1x" id="addPic1x">
         <input type="hidden" name="addPic1y" id="addPic1y">
         <input type="hidden" name="addPic2x" id="addPic2x">
