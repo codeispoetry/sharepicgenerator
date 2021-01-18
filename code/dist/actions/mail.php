d7413ba2c245a413387f0c7a51be75241b0b607e
Tom Rose <mail@tom-rose.de>
2021-01-18 20:55:06 +0100

    fix frankfurt highlightcolor

--- code/dist/actions/mail.php
+++ code/dist/actions/mail.php
@@ -14,5 +14,19 @@ $file = getBasePath('tmp/' . sanitizeUserinput($_POST['file']));
 $recipient = $_POST['recipient'];
 $text = $_POST['text'];
 
+$db = new SQLite3(getBasePath('log/logs/log.db'));
+if (isAdmin()) {
+    $db->exec('CREATE TABLE IF NOT EXISTS mail(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            user TEXT,
+            recipient TEXT)');
+}
+
+$smt = $db->prepare('INSERT INTO mail (user,recipient) values (:user, :recipient)');
+$smt->bindValue(':user', getUser(), SQLITE3_TEXT);
+$smt->bindValue(':recipient', $recipient, SQLITE3_TEXT);
+
+$smt->execute();
 
 email($recipient, $file, $text);
--- code/dist/tenants/cockpit/addtext-none.php
+++ code/dist/tenants/cockpit/addtext-none.php
@@ -1,25 +1,5 @@
-<h3 class="collapsed d-none" data-toggle="collapse" data-target=".addtext"><i class="fa fa-asterisk"></i> Sternchentext</h3>
-<div class="addtext d-none list-group-item list-group-item-action flex-column align-items-start collapse">
-    <div class="mb-1 list-group-item-content">
-        <div class="d-flex align-items-lg-center">
-            <textarea name="addtext" id="addtext" placeholder="Sternchentext" value="" class="form-control"></textarea>
-        </div>
-        <div class="d-flex justify-content-between">
-            <div class="slider">
-                <small>klein</small>
-                <input type="range" class="custom-range" name="addtextsize" id="addtextsize" min="0"
-                    max="50" value="20">
-                <small>groß</small>
-            </div>
-            <div>
-                <span class="to-front" data-target="addtext" title="Sternchentext nach vorne">
-                    <i class="fas fa-layer-group text-primary"></i>
-                </span> 
-                <i class="fa fa-broom ml-1 text-primary cursor-pointer addtext-change-color ml-1" title="Farbe wechseln"></i>
-            </div>
-        </div>    
-    </div>
-</div>
-
+<textarea name="addtext" id="addtext" placeholder="Sternchentext" value="" class="form-control"></textarea>
+<input type="range" class="custom-range" name="addtextsize" id="addtextsize" min="0" max="50" value="20">
+<input type="hidden" name="addtextColor" id="addtextColor" value="#000000">
 <input type="hidden" name="addtextX" id="addtextX">
 <input type="hidden" name="addtextY" id="addtextY">
