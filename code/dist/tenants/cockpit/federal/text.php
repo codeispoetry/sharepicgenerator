65c018784af899c0cbfb2e9eb4000c4137a6bf62
Tom Rose <mail@tom-rose.de>
2021-01-20 22:06:07 +0100

    introduce sayings

--- code/dist/tenants/cockpit/federal/text.php
+++ code/dist/tenants/cockpit/federal/text.php
@@ -19,15 +19,14 @@
 
             <div class="list-group-item-content">
                 <div class="">
-                    <input type="text" placeholder="Text über der Linie" name="textbefore" id="textbefore" value="Sharepicgenerator.de"
+                    <input type="text" placeholder="Text über der Linie" name="textbefore" id="textbefore" value="<?php echo getSaying('upper'); ?>"
                            class="form-control showonly lines nolines">
                 </div>
                 <div class="">
-                    <textarea placeholder="Haupttext" name="text" id="text" class="form-control">Es beginnt
-#mitdir.</textarea>
+                    <textarea placeholder="Haupttext" name="text" id="text" class="form-control"><?php echo getSaying('main'); ?></textarea>
                 </div>
                 <div class="d-flex align-items-lg-center">
-                    <input type="text" placeholder="Text unter der Linie" name="textafter" id="textafter" value="" class="form-control showonly lines nolines quote">
+                    <input type="text" placeholder="Text unter der Linie" name="textafter" id="textafter" value="<?php echo getSaying('lower'); ?>" class="form-control showonly lines nolines quote">
                 </div>
 
                 <div class="mb-1 mt-2">
