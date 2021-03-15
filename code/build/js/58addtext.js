dfb1e50f43944325413a7a6ad2daca00e8696e39
Tom Rose <mail@tom-rose.de>
2021-03-15 08:48:37 +0100

    preserve white space in add text

--- code/build/js/58addtext.js
+++ code/build/js/58addtext.js
@@ -26,6 +26,10 @@ const addtext = {
     const addtextContent = draw.text($('#addtext').val())
       .font(Object.assign(addtext.font, { size: fontSize }))
       .fill($('#addtextColor').val());
+
+    addtextContent.attr('xml:space', 'preserve');
+    addtextContent.attr('style', 'white-space:pre');
+
     addtext.svg.add(addtextContent);
 
     eraser.front();
@@ -37,3 +41,4 @@ const addtext = {
 };
 
 $('#addtext, #addtextsize').bind('input propertychange', addtext.draw);
+console.log("HI");
