3649980a70838a1172157681ca6643a293fc385c
Tom Rose <mail@tom-rose.de>
2021-03-27 21:13:05 +0100

    improve basic text

--- code/build/js/basic/52basic.js
+++ code/build/js/basic/52basic.js
@@ -1,123 +1,41 @@
-/* eslint-disable no-undef */
 const basic = {
   svg: draw.text(''),
-  grayBackground: draw.circle(0),
-  colors: ['#ff0000', '#ffee00'],
   lineheight: 20,
   linemargin: -4,
   paddingLr: 5,
   font: {
-    anchor: 'left',
     leading: '1.0em',
     size: 20,
   },
-  fontoutsidelines: {
-    family: 'ArvoGruen',
-    size: 8,
-    anchor: 'left',
-    leading: '1.0em',
-  },
 
   draw() {
-    if (config.layout !== 'basic') {
-      return;
-    }
-
     config.noBackgroundDragAndDrop = false;
 
-    text.svg.remove();
-    invers.svg.remove();
-    invers.backgroundClone.remove();
-    if ($('#text').val() === '') return;
+    basic.svg.remove();
 
-    text.svg = draw.group().attr('id', 'svg-text');
+    basic.svg = draw.group().attr('id', 'svg-baisctext').addClass('draggable').draggable();
 
-    textDragging();
-
-    let y = 0;
-
-    // let lines = '„' + $('#text').val() + '“';
-
-    let lines = $('#text').val().toUpperCase();
-    const quotationMarks = ['„', '“'];
-    let qmI = 0;
-    while ((lines.match(/"/g) || []).length) {
-      lines = lines.replace(/"/, quotationMarks[qmI]);
-      qmI = (qmI + 1) % 2;
-    }
-
-    lines = lines.replace(/\n$/, '').split(/\n/);
-    const fontfamily = 'ArvoGruen';
-
-    const lineBeginsY = [];
-    const linesRendered = [];
-    let color;
-
-    lines.forEach((value, index) => {
-      let style = 1;
-
-      // the main text
-      const values = value.split(/\[|\]/);
-
-      const t = draw.text((add) => {
-        for (let i = 0; i < values.length; i++) {
-          style = (style === 0) ? 1 : 0;
-
-          color = basic.colors[style];
-          if (style === 0) {
-            color = textColors[$('#textColor').val()];
-          }
-
-          add.tspan(values[i]).fill(color).font(
-            Object.assign(basic.font, { family: fontfamily }),
-          );
-
-          add.attr('xml:space', 'preserve');
-          add.attr('style', 'white-space:pre');
-        }
-      });
-
-      t.y(y);
-
-      y += (t.rbox().h) + basic.linemargin;
-
-      lineBeginsY[index] = y;
-      linesRendered[index] = t;
-      text.svg.add(t);
+    basic.svg.on('dragend.namespace', function dragEnd() {
+      $('#textX').val(Math.round(this.x()));
+      $('#textY').val(Math.round(this.y()));
     });
 
-    // add lower line
-    let lineafter;
-    if ($('#textafter').val().length > 0) {
-      lineafter = draw.rect(10, 10)
-        .fill('#E6007E').dy(text.svg.height());
-      text.svg.add(lineafter);
-    }
+    const size = parseInt($('#textsize').val(), 10);
+    const family = $('#textfont').val();
+    const anchor = $('#textanchor').val();
 
-    // text below the line
-    // text above the line
-    eraser.front();
+    const basicContent = draw.text($('#text').val())
+      .font(Object.assign(basic.font, { family, size, anchor }))
+      .fill($('#textcolor').val());
 
+    basicContent.attr('xml:space', 'preserve');
+    basicContent.attr('style', 'white-space:pre');
 
-    // gray layer behind text
-    text.grayBackground.remove();
-    if ($('#graybehindtext').prop('checked')) {
-      const grayGradient = draw.gradient('radial', (add) => {
-        add.stop({ offset: 0, color: $('#colorbehindtext').val(), opacity: 0.9 });
-        add.stop({ offset: 0.9, color: $('#colorbehindtext').val(), opacity: 0.0 });
-      });
-      grayGradient.from(0.5, 0.5).to(0.5, 0.5).radius(0.5);
+    basic.svg.add(basicContent);
 
-      text.grayBackground = draw.rect(text.svg.width(), text.svg.height())
-        .fill({ color: grayGradient, opacity: 0.3 })
-        .back();
-    }
-
-    text.svg.move(parseInt($('#textX').val(), 10), parseInt($('#textY').val(), 10)).size(parseInt($('#textsize').val(), 10));
-    text.positionGrayBackground();
-    checkForOtherTenant();
+    basic.svg.move(parseInt($('#textX').val(), 10), parseInt($('#textY').val(), 10));
   },
 
 };
 
-$('#text, #textafter, #textbefore, #textsize, #graybehindtext').bind('input propertychange', basic.draw);
+$('#text, #textsize, #textfont, #textanchor').bind('input propertychange', basic.draw);
