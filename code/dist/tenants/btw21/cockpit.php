00b22d5d2edec6813f0ebe82dcb52ba263e048d9
Tom Rose <mail@tom-rose.de>
2021-06-06 22:46:44 +0200

    introduce btw21

---
+++ code/dist/tenants/btw21/cockpit.php
@@ -0,0 +1,25 @@
+<?php
+// phpcs:ignoreFile -- mainly html, ignore it
+?>
+
+<form id="pic">
+    <div class="mb-5">
+    <?php
+        require_once(getBasePath('tenants/cockpit/picture.php'));
+        require_once(getBasePath('tenants/cockpit/picture-size.php'));
+        require_once(getBasePath('tenants/cockpit/federal/text.php'));
+        require_once(getBasePath('tenants/cockpit/addpictures.php'));
+        require_once(getBasePath('tenants/cockpit/federal/logo.php'));
+        require_once(getBasePath('tenants/cockpit/federal/eyecatcher.php'));
+        require_once(getBasePath('tenants/cockpit/addtext.php'));
+        require_once(getBasePath('tenants/cockpit/eraser.php'));
+        require_once(getBasePath('tenants/cockpit/quality.php'));
+        require_once(getBasePath('tenants/cockpit/workfile.php'));
+        require_once(getBasePath('tenants/cockpit/gallery.php'));
+        require_once(getBasePath('tenants/cockpit/mail.php'));
+        require_once(getBasePath('tenants/cockpit/gridlines.php'));
+        require_once(getBasePath('tenants/cockpit/footer.php'));
+    ?>
+
+    </div>
+</form>
