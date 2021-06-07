a8aba5c0f7e3f743d14e6f24b4a64e7fed7ec921
Tom Rose <mail@tom-rose.de>
2021-06-07 22:35:49 +0200

    fix colorlayer backness

--- code/build/js/28background.js
+++ code/build/js/28background.js
@@ -78,6 +78,7 @@ const background = {
     this.darklightlayer.back();
     this.greenlayer.back();
     this.svg.back();
+    this.colorlayer.back();
   },
 
   addDarkLightLayer() {
@@ -94,6 +95,7 @@ const background = {
     this.darklightlayer.back();
     this.greenlayer.back();
     this.svg.back();
+    this.colorlayer.back();
   },
 
   addFilter() {
