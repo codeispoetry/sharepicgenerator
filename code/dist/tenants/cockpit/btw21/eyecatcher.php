5d1b42b644cf35cfa98ae79702a398bb87368d0f
Tom Rose <mail@tom-rose.de>
2021-06-07 14:13:02 +0200

    add eyecatcher templates for btw21

--- code/dist/tenants/cockpit/btw21/eyecatcher.php
+++ code/dist/tenants/cockpit/btw21/eyecatcher.php
@@ -1,14 +1,25 @@
 <h3 class="collapsed" data-toggle="collapse" data-target=".eyecatcher"><i class="far fa-eye"></i> Störer</h3>
-<div class="eyecatcher list-group-item list-group-item-action flex-column align-items-start collapse">
+<div class="eyecatcher list-group-item list-group-item-action flex-column align-items-start collapseKILL">
     <div class="mb-1 list-group-item-content">
+        <div class="">
+            <select class="form-control" id="eyecatchertemplate">
+                <option value="">Vorlage wählen</option>
+                <option value="custom">Text selbt eingeben</option>
+                <optgroup label="Vorlagen">
+                    <option value="alle-stimmen.png">Alle Stimmen Grün</option>
+                    <option value="breifwahl.png">Briefwahl jetzt!</option>
+                </optgroup>
+            </select>
+            oder
+        </div>
         <div class="d-flex align-items-lg-center">
-            <textarea name="pintext" id="pintext" placeholder="" class="form-control" data-maxlines="3"></textarea>
+            <textarea name="pintext" id="pintext" placeholder="Dein Text" class="form-control" data-maxlines="3"></textarea>
         </div>
         <div class="d-flex justify-content-between">
             <div class="slider">
                 <small>klein</small>
                 <input type="range" class="custom-range" name="eyecatchersize" id="eyecatchersize" min="50"
-                    max="300" value="100" disabled>
+                    max="300" value="100">
                 <small>groß</small>
             </div>
             <div>
