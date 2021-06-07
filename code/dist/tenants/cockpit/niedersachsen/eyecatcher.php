dd77b80053cc47af002fd66834cfd70eb10bd8bd
Tom Rose <mail@tom-rose.de>
2021-06-07 13:23:58 +0200

    nds: add eyecatcher templates

---
+++ code/dist/tenants/cockpit/niedersachsen/eyecatcher.php
@@ -0,0 +1,36 @@
+<h3 class="collapsed" data-toggle="collapse" data-target=".eyecatcher"><i class="far fa-eye"></i> Störer</h3>
+<div class="eyecatcher list-group-item list-group-item-action flex-column align-items-start collapseKILL">
+    <div class="mb-1 list-group-item-content">
+        <div class="">
+            <select class="form-control" id="eyecatchertemplate">
+                <option value="">Vorlage wählen</option>
+                <option value="custom">Text selbt eingeben</option>
+                <optgroup label="Vorlagen">
+                    <option value="datum.png">12.9 und 26.9</option>
+                    <option value="alle-stimmen.png">Alle Stimmen Grün</option>
+                    <option value="breifwahl.png">Briefwahl jetzt!</option>
+                </optgroup>
+            </select>
+            oder
+        </div>
+        <div class="d-flex align-items-lg-center">
+            <textarea name="pintext" id="pintext" placeholder="Dein Text" class="form-control" data-maxlines="3"></textarea>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="slider">
+                <small>klein</small>
+                <input type="range" class="custom-range" name="eyecatchersize" id="eyecatchersize" min="50"
+                    max="300" value="100">
+                <small>groß</small>
+            </div>
+            <div>
+                <span class="to-front" data-target="pin" title="Störer nach vorne">
+                    <i class="fas fa-layer-group text-primary"></i>
+                </span> 
+            </div>
+        </div>    
+    </div>
+</div>
+
+<input type="hidden" name="pinX" id="pinX">
+<input type="hidden" name="pinY" id="pinY">
