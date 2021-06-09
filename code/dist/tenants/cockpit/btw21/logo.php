a185f58e13544ec95a1d3a357622a1874481c344
Tom Rose <mail@tom-rose.de>
2021-06-10 00:14:37 +0200

    add more function

--- code/dist/tenants/cockpit/btw21/logo.php
+++ code/dist/tenants/cockpit/btw21/logo.php
@@ -1,5 +1,5 @@
-<h3 class="collapsed expertmode" data-toggle="collapse" data-target=".logo"><i class="fas fa-fan"></i> Logo</h3>
-        <div class="logo expertmode collapse list-group-item list-group-item-action flex-column align-items-start">           
+<h3 class="collapsed" data-toggle="collapse" data-target=".logo"><i class="fas fa-fan"></i> Logo</h3>
+        <div class="logo collapse list-group-item list-group-item-action flex-column align-items-start">           
             <div class="d-flex justify-content-between">
                 <div class="slider">
                     <small>klein</small>
