4fe7bff173f567c5e0000efc8414aae57f827d10
Tom Rose <mail@tom-rose.de>
2021-06-18 10:50:55 +0200

    toggle line height manually

--- code/build/js/btw21/50alltext.js
+++ code/build/js/btw21/50alltext.js
@@ -38,9 +38,25 @@ function showActionDayHint() {
 
 // eslint-disable-next-line no-unused-vars
 function setLineHeight() {
+  if (config.lineHeightToggledManually) {
+    return;
+  }
+
   if (/Ä|Ö|Ü|g|j|p|q|y/.test($('#text').val())) {
     nolines.font.leading = '1.17em';
   }else{
     nolines.font.leading = '1.05em';
   }
 }
+
+$('.toggle-line-height').click(() =>{
+  config.lineHeightToggledManually = true;
+  if (nolines.font.leading == '1.05em') {
+    nolines.font.leading = '1.17em';
+  } else {
+    nolines.font.leading = '1.05em';
+  }
+
+  area.draw();
+  nolines.draw();
+});
