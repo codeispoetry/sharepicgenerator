2a54f98615a880c3e10dd38db689f6d02ce5be7d
Tom Rose <mail@tom-rose.de>
2021-06-18 10:50:55 +0200

    toggle line height manually

--- code/dist/tenants/cockpit/btw21/text.php
+++ code/dist/tenants/cockpit/btw21/text.php
@@ -15,6 +15,7 @@
 
             <div class="list-group-item-content">
                 <div class="d-flex justify-content-end">
+                    <i class="fa fa-text-height toggle-line-height me-2" title="Zeilenabstand ändern"></i>
                     <i class="fa fa-align-left text-align me-2" data-align="left" title="linksbündig"></i>
                     <i class="fa fa-align-center text-align me-2" data-align="middle" title="zentrieren"></i>
                     <i class="fa fa-align-right text-align" data-align="end" title="rechtsbündig"></i>
