2f0d6ff74f349a6254df8c0f767fd2c28ccd09fe
Tom Rose <mail@tom-rose.de>
2021-06-22 01:37:18 +0200

    refactor

--- code/dist/tenants/cockpit/btw21/logo.php
+++ code/dist/tenants/cockpit/btw21/logo.php
@@ -1,18 +1,18 @@
-<h3 class="collapsed" data-toggle="collapse" data-target=".logo"><i class="fas fa-fan"></i> Logo</h3>
-        <div class="logo collapse list-group-item list-group-item-action flex-column align-items-start">           
-            <div class="d-flex justify-content-between">
-                <div class="slider">
-                    <small>klein</small>
-                        <input type="range" class="form-range" name="logosize" id="logosize" min="1" max="100" value="20">
-                    <small>groß</small>
-                </div>
-                <div>
-                    <span class="to-front" data-target="logo" title="Logo nach vorne">
-                        <i class="fas fa-layer-group text-primary"></i>
-                    </span> 
-                </div>
-            </div>
-           
+<h3 class="collapsed showonly floating" data-toggle="collapse" data-target=".logo"><i class="fas fa-fan"></i> Logo</h3>
+<div class="logo collapse list-group-item list-group-item-action flex-column align-items-start showonly floating">           
+    <div class="d-flex justify-content-between">
+        <div class="slider">
+            <small>klein</small>
+                <input type="range" class="form-range" name="logosize" id="logosize" min="1" max="100" value="20">
+            <small>groß</small>
         </div>
-        <input type="hidden" name="logoX" id="logoX" value="380">
-        <input type="hidden" name="logoY" id="logoY" value="220">
+        <div>
+            <span class="to-front" data-target="logo" title="Logo nach vorne">
+                <i class="fas fa-layer-group text-primary"></i>
+            </span> 
+        </div>
+    </div>
+    
+</div>
+<input type="hidden" name="logoX" id="logoX" value="380">
+<input type="hidden" name="logoY" id="logoY" value="220">
