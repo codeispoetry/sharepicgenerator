12d5c6dc52fd6df4da2192cebceb937963cf0c1a
Tom Rose <mail@tom-rose.de>
2021-07-09 09:47:43 +0200

    update faq

--- code/dist/lib/overlays/faq.php
+++ code/dist/lib/overlays/faq.php
@@ -10,23 +10,23 @@
                 <h5>Wer darf den Sharepicgenerator nutzen?</h5>
                 <p>Jedes Mitglied der Grünen darf den Sharepicgenerator im Sinne unseres Grundsatzprogrammes nutzen.</p>
 
-                <h5>Kann ich auch ein eigenes Logo, z.B. der grünen Jugend hochladen?</h5>
-                <p>Ja, unter <em>Logo</em> kannst Du beliebig viele eigene Logos hochladen.</p>
-
+            
                 <h5>Zur Landtagswahl haben wir eine eigene Kampagne. Kann ich dafür auch Sharepic erstellen?</h5>
-                <p>Es gibt regionale Angebote für einzelne Bundesländer und Kreisverbände. Diese findest Du oben im Menü.</p>
+                <p>Es gibt regionale Angebote für einzelne Bundesländer und Kreisverbände. Diese findest Du oben im Menü
+                    unter <em>Regionale Angebote</em>.
+                </p>
 
                 <h5>Kann ich die Sharepics auch ausdrucken?</h5>
-                <p>Ja. Unter <em>Größe</em> kannst Du eine Vorlage, z.B. für eine Postkarte auswählen oder die Größe beliebig einstellen.</p>
+                <p>Ja. Unter <em>Bild -> Größe</em> kannst Du eine Vorlage, z.B. für eine Postkarte auswählen oder die Größe beliebig einstellen.</p>
 
                 <h5>Ich habe kein passendes Hintergrundbild. Wo bekomme ich Bilder her?</h5>
                 <p>
-                    Bei <em>Hauptbild</em> gibt es eine Suchmaske. Aktuell werden drei verschiedene Bilddatenbanken durchsucht. 
+                    Bei <em>Bild</em> gibt es eine Suchmaske. Aktuell werden drei verschiedene Bilddatenbanken durchsucht. 
                     Deren Bilder darfst Du kostenlos nutzen.
                 </p>
-                <h5>Ich kann im Bundeslayout das Logo nicht mehr austauschen und keine Zusatzbilder mehr laden</h5>
+                <h5>Wo kann ich im Bundeslayout das Logo austauschen?</h5>
                 <p>
-                    Klicke links unten auf <em>Mehr Funktionen</em>, dann stehen Dir diese Funktionen wieder zur Verfügung.
+                    Klicke links unten auf <em>Einstellungen</em>, dann auf <em>Logos bearbeiten</em>.
                 </p>
             </div>
         </div> 
