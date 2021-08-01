7dba1b1af407d878295e90ae5bf046ae6c30111e
Tom Rose <mail@tom-rose.de>
2021-08-01 17:18:15 +0200

    set message disable worfile

--- code/dist/tenants/cockpit/workfile.php
+++ code/dist/tenants/cockpit/workfile.php
@@ -1,12 +1,18 @@
 
         <h3><i class="fas fa-wrench"></i> Arbeitsdatei</h3>
         <div class="workfile list-group-item list-group-item-action flex-column align-items-start">
-            <div>
-                Mit der Arbeitsdatei kannst Du Dein Sharepic später weiter bearbeiten.
-            </div>
-            <div>
-                <button type="button" class="btn btn-info btn-sm" id="savework" data-click="savework"><i class="fas fa-download"></i> herunterladen</button>
-                <button type="button" class="btn btn-info btn-sm uploadworkclicker" id="uploadworkclicker"><i class="fas fa-upload"></i> zip-Datei hochladen</button>
+            <em class="small">
+                Wegen anhaltender technischer Problem steht diese Funktion nicht mehr zur Verfügung.
+                Ich bitte um Entschuldigung.
+            </em>
+            <div class="d-none">
+                <div>
+                    Mit der Arbeitsdatei kannst Du Dein Sharepic später weiter bearbeiten.
+                </div>
+                <div>
+                    <button type="button" class="btn btn-info btn-sm" id="savework" data-click="savework"><i class="fas fa-download"></i> herunterladen</button>
+                    <button type="button" class="btn btn-info btn-sm uploadworkclicker" id="uploadworkclicker"><i class="fas fa-upload"></i> zip-Datei hochladen</button>
+                </div>
             </div>
         </div>
         <input type="file" class="custom-file-input upload-file" id="uploadwork" accept="application/zip">
