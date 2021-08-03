35aeff5a4aae5fd6a7f2a386b9723f1ceae9a560
Tom Rose <mail@tom-rose.de>
2021-08-03 23:18:22 +0200

    refactor cockpit bw

--- code/dist/tenants/cockpit/bw/eyecatcher.php
+++ code/dist/tenants/cockpit/bw/eyecatcher.php
@@ -1,5 +1,5 @@
-<h3 class="collapsed" data-toggle="collapse" data-target=".eyecatcher"><i class="far fa-eye"></i> Störer</h3>
-        <div class="eyecatcher list-group-item list-group-item-action flex-column align-items-start collapse">
+<h3><i class="far fa-eye"></i> Störer</h3>
+        <div class="eyecatcher list-group-item list-group-item-action flex-column align-items-start ">
             <div class="mb-1 list-group-item-content">
                 <div class="d-flex align-items-lg-center">
                     <textarea name="pintext" id="pintext" placeholder="Störertext. Maximal 2 Zeilen." value="" class="form-control height1line"></textarea>
