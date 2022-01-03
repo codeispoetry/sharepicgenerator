8112e25ec8b22841071207985726afe91d4df351
Tom Rose <mail@tom-rose.de>
2022-01-03 23:20:18 +0100

    introduce nrw, sh

---
+++ code/build/js/sh/57area.js
@@ -0,0 +1,174 @@
+/* eslint-disable no-undef */
+const area = {
+  svg: draw.text(''),
+  fond: draw.circle(0),
+  fondPadding: 30,
+  font: {
+    family: 'BereitBold',
+    anchor: 'left',
+    leading: '1.05em',
+    size: 20,
+  },
+  fontBefore: {
+    family: 'BereitBold',
+    anchor: 'left',
+    leading: '1.05em',
+    size: 10,
+  },
+  fontAfter: {
+    family: 'BereitBold',
+    anchor: 'left',
+    leading: '1.05em',
+    size: 10,
+  },
+
+  draw() {
+    if (config.layout !== 'area'
+       || $('#text').val() === '') {
+      return;
+    }
+
+    if (typeof floating === 'object' && floating !== null) {
+      floating.hide();
+    }
+
+    area.svg.remove();
+    area.svg = draw.group();
+    logo.svg.draggable(false);
+
+    setLineHeight();
+
+    const t = draw.text($('#text').val())
+      .font(area.font)
+      .fill('#FFFFFF')
+      .attr('xml:space', 'preserve')
+      .attr('style', 'white-space:pre');
+
+    if ($('#textbefore').val()) {
+      area.svg.add(area.drawTextBefore());
+    }
+
+    if ($('#textafter').val()) {
+      area.svg.add(area.drawTextAfter(t));
+    }
+
+    area.svg.add(t);
+
+    if ($('#showclaim').prop('checked')) {
+      area.drawClaim();
+    }
+
+    area.svg
+      .size($('#textsize').val())
+      .move(area.fondPadding, draw.height() - area.getLowerFondCorrection() - area.fondPadding);
+
+    eraser.front();
+
+    area.drawFond();
+    area.svg.front();
+    copyright.front();
+    pin.front();
+    window.setTimeout(area.drawLogo, 500);
+  },
+
+  drawClaim() {
+    return claim.svg
+      .clone()
+      .addTo(area.svg)
+      .front()
+      .show()
+      .move(-3, 5 + area.svg.height() + area.svg.y());
+  },
+
+  drawTextBefore() {
+    const textbefore = draw.text($('#textbefore').val())
+      .font(area.fontBefore)
+      .fill('#FFE100')
+      .attr('xml:space', 'preserve')
+      .attr('style', 'white-space:pre');
+
+    textbefore.move(0, -textbefore.bbox().h);
+
+    switch (area.align) {
+      case 'middle':
+        textbefore.x(-textbefore.bbox().w / 2);
+        break;
+      case 'end':
+        textbefore.x(-textbefore.bbox().w);
+        break;
+      default:
+    }
+
+    return textbefore;
+  },
+
+  drawTextAfter(t) {
+    claim.svg.hide();
+    return draw.text($('#textafter').val())
+      .font(area.fontAfter)
+      .fill('#FFFFFF')
+      .move(0, 8 + t.bbox().height)
+      .attr('xml:space', 'preserve')
+      .attr('style', 'white-space:pre');
+  },
+
+  getLowerFondCorrection() {
+    let height = area.svg.height();
+
+    if ($('#showclaim').prop('checked') === true) {
+      return height;
+    }
+
+    text = $('#text').val();
+    heihgtCorrection = 0.09;
+    if ($('#textafter').val() && $('#showclaim').prop('checked') === false) {
+      text = $('#textafter').val();
+      heihgtCorrection = 0.03;
+    }
+
+    if (!lastLineHasDescender(text)) {
+      height -= height * heihgtCorrection;
+    }
+
+    return height;
+  },
+
+  getUpperFondCorrection() {
+    let height = area.svg.height();
+
+    if (firstLineHasAscender($('#text').val())) {
+      height = 0;
+    } else {
+      height *= 0.09;
+    }
+
+    return height;
+  },
+
+  drawFond() {
+    area.fond.remove();
+    const h = area.getLowerFondCorrection() - area.getUpperFondCorrection()
+        + (2 * area.fondPadding);
+    area.fond = draw.rect(draw.width(), h)
+      .move(0, draw.height() - h)
+      .fill('#a0c864');
+  },
+
+  drawLogo() {
+    const size = area.fond.height() * 0.7;
+    logo.svg
+      .size(size, size)
+      .move(draw.width() - (size * 1.2), draw.height() - area.fond.height() - (size * 0.7))
+      .removeClass('draggable')
+      .draggable(false)
+      .front();
+  },
+
+  hide() {
+    area.svg.hide();
+    area.fond.hide();
+  },
+
+};
+
+$('#text, #textafter, #textbefore, #textsize, #graybehindtext, #showclaim').bind('input propertychange', area.draw);
