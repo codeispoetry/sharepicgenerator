57e6d88a8b3a1da3d6f21802fbbd003898a2164c
Thomas Rose <t.rose@seidemann-web.com>
2022-01-25 12:15:57 +0100

    introduce new design for startpage

--- code/dist/index.php
+++ code/dist/index.php
@@ -25,37 +25,77 @@
 
     <style>
     body {
-      background-image: linear-gradient(180deg, #eee, #fff 100px, #fff);
+
     }
 
     .container {
       max-width: 960px;
     }
 
-    .pricing-header {
-      max-width: 700px;
+    footer h5{
+        color: white;
+        font-family: BereitBold;
+        font-size: 1.5em;
     }
 
-    .link-secondary{
-      color: #6c757d;
+    footer a{
+        text-decoration: none;
     }
-    .link-secondary:hover{
-      color: #46962b;
+    .bgimage{
+        background: url(assets/startimage.jpg);
+        background-size: cover;
+        min-height: 600px;
+        position: relative;
+     }
+
+    #loginscreen{
+        width: 400px;
+        border-radius: 10px;
+        padding: 2em;
+        position: absolute;
+        right: 30px;
+        bottom: 30px;
+        background: rgba(255,255,255, 0.9);
     }
 
-    .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
+    #claim{
+        position: absolute;
+        left: 30px;
+        bottom: 30px;
+        color: white;
+        font-family: BereitBold;
+    }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+    #claim h1{
+        font-family: BereitBold;
+        font-size: 7em;
+    }
+
+    .subhead{
+        font-size: 5em;
+    }
+
+      @media (max-width: 768px) {
+          .bgimage{
+              min-height: 230px;
+          }
+        #loginscreen{
+            position: relative;
+            top: 10px;
+            right: unset;
+            width: 100%;
+        }
+        #claim{
+            display: none;
         }
       }
+
+        @media (max-width: 1024px) {
+            #claim {
+                max-width: 500px;
+            }
+        }
+    }
     </style>
 </head>
 <?php
@@ -63,132 +103,90 @@
   require_once('lib/functions.php');
   require_once('lib/log_functions.php');
 
+  readConfig();
+
 
 ?>
-<body>
-<div class="container py-3">
-  <header class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-      <img class="me-2" src="assets/img/logo.svg" alt="" width="40" height="40">
-      <span class="fs-4">Sharepicgenerator.de</span>
-    </a>
-  </header>
-
-  <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-    <h1 class="display-4 fw-normal">Sharepic&shy;generator</h1>
-    <p class="fs-5 text-muted">Erstelle Bilder mit Text für Social Media und Co. </p>
-  </div>
-
-  <main>
-    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center ">
-      <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm small">
-          <div class="card-header py-3">
-            <h5 class="my-0 fw-normal">Ohne Anmeldung</h5>
-          </div>
-          <div class="card-body">
-            <ul class="list-unstyled mt-3 mb-4 d-none d-md-block">
-              <li>Logo hochladen</li>
-              <li>Schrift hochladen</li>
-              <li>Hintergrundbild aussuchen</li>
-              <li>Text eingeben</li>
-              <li>herunterladen</li>
-            </ul>
-            <a href="/tenants/basic/?guest=1" class="w-100 btn btn-sm btn-dark">Sharepic erstellen</a>
-            &nbsp;
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm border-pistazie">
-          <div class="card-header py-3 text-white bg-pistazie border-pistazie">
-            <h4 class="my-0 fw-normal">
-                B'90/Grüne
-              <img src="/assets/logos/sonnenblume.svg" style="height: 1em;margin-left: 10px" >
-            </h4>
-          </div>
-          <div class="card-body" style="background-color: #f3f8f1">
-            <ul class="list-unstyled mt-3 mb-4 d-none d-md-block">
-              <li class="mb-5">im grünen Design</li>
-    
-              <li> 
-                <a href="/btw21">Bundestagswahl</a> |
-                <a href="/tenants/federal">Bundeslayout</a>
-                
-
-                <br>
-                <small>
-                  <a href="/tenants/bw" class="small">Ba-Wü</a> |
-                  <a href="/tenants/berlin">Berlin</a> |
-                  <a href="/tenants/frankfurt" class="small">Frankfurt</a>
-                </small>
-              </li>
-            </ul>
-            <a href="tenants/btw21" type="button" class="w-100 btn btn-lg btn-pistazie">einloggen</a>
-            <a href="https://www.gruene.de/mitglied-werden" target="_blank" class="">Mitglied werden</a>
-
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm small">
-          <div class="card-header py-3">
-            <h5 class="my-0 fw-normal">Eigenes Design</h5>
-          </div>
-          <div class="card-body">
-            <ul class="list-unstyled mt-3 mb-4 d-none d-md-block">
-              <li>Logo speichern</li>
-              <li>Schrift speichern</li>
-              <li>Störer bestimmen</li>
-              <li>Sternchentext formatieren</li>
-              <li>Zusatzbilder nutzen</li>
-            </ul>
-            <a href="/tenants/basic" class="w-100 btn btn-sm btn-info">frei anmelden</a>
-            <a href="/wordpress/wp-login.php?action=register" class="text-info">frei registrieren</a>
-            | <a href="/tenants/federal">B' 90 / Die Grünen</a>
-          </div>
-        </div>
-      </div>
+<body >
+<div class="container-fluid p-0">
+  <header class="container d-flex justify-content-between">
+    <div class="my-auto">
+        <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+            <img class="me-2" src="assets/img/logo.svg" alt="" width="40" height="40">
+            <span class="fs-4">Sharepicgenerator.de</span>
+        </a>
+    </div>
+    <div class="btn-pistazie">
+        <a href="tenants/btw21" type="button" class="btn btn-md btn-pistazie p-4">
+            <i class="fas fa-sign-in-alt me-1"></i> einloggen</a>
     </div>
 
-  </main>
 
-  <footer class="pt-4 my-md-5 pt-md-5 border-top position-relative">
-    <div class="row">
+  </header>
+
+  <div class="p-3 bgimage">
+      <div id="claim">
+          <h1 class="">Sharepic&shy;generator</h1>
+          <p class="subhead">Werde kreativ!</p>
+      </div>
+      <div id="loginscreen">
+          <h2 class="display-6 fw-normal bereitbold">Sharepic&shy;generator</h2>
+          <p class="fs-5">Erstelle Bilder mit Text für Social Media und Co. im grünen Design </p>
+          <a href="tenants/btw21" type="button" class="w-100 btn btn-lg btn-primary">einloggen</a>
+      </div>
+  </div>
+
+
+
+  <footer class="bg-primary p-5 position-relative">
+      <div class="container">
+         <div class="row">
       <div class="col-6 col-md">
         <h5>Über</h5>
         <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="/imprint.php">Impressum</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/codeispoetry/sharepicgenerator">Quellcode</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none d-none" href="/organizations.php">Für Organisationen</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="MAILTO:tom.rose@sharepicgenerator.de">Kontakt</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="/imprint.php">Datenschutz</a></li>
+          <li class="mb-1"><a class="" href="/imprint.php">Impressum</a></li>
+
+          <li class="mb-1"><a class=" d-none" href="/organizations.php">Für Organisationen</a></li>
+          <li class="mb-1"><a class="" href="MAILTO:tom.rose@sharepicgenerator.de">Kontakt</a></li>
+          <li class="mb-1"><a class="" href="/imprint.php">Datenschutz</a></li>
 
         </ul>
       </div>
       <div class="col-6 col-md text-md-center">
-        <h5>Features</h5>
+        <h5>Sondermodelle</h5>
         <ul class="list-unstyled text-small">
-          <li class="mb-1">Anpassbare Ausgabegröße</li>
-          <li class="mb-1">Bildausschnitt frei wählbar</li>
-          <li class="mb-1">für Bilder und Videos</li>
-          <li class="mb-1">eigenes Logo</li>
-          <li class="mb-1">eigene Schriftarten</li>
+            <?php
+            $tenants = configValue('Main','linkedTenants');
+            foreach ($tenants as $key => $value ){
+                printf('<li class="mb-1"><a href="/%1$s" class="">%2$s</a></li>',
+                    $key,
+                    $value
+                );
+            }
+            ?>
+
+            <li class="mb-1 mt-2"><a href="/tenants/basic/?guest=1" class="">freies Sharepic erstellen</a></li>
+
+            <li class="mb-1 d-none"><a href="/tenants/basic" class="">frei anmelden</a></li>
+          <li class="mb-1 d-none"><a href="/wordpress/wp-login.php?action=register" class="">frei registrieren</a></li>
+        <li>
+            <form method="post" class="test-access" action="tenants/federal/">
+                <input type="password" id="test-access-password" class="" name="pass" placeholder="Gastzugang">
+            </form>
+        </li>
         </ul>
       </div>
-      <div class="col-6 col-md text-md-right">
+      <div class="col-6 col-md text-md-end">
         <h5>Hilfen</h5>
         <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="/documentation">Handbuch</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/codeispoetry/sharepicgenerator/issues">Fehler melden</a></li>
-          <li>
-          <form method="post" class="test-access" action="tenants/federal/">
-            <input type="password" id="test-access-password" class="" name="pass" placeholder="Gastzugang">
-          </form>
-          </li>
+          <li class="mb-1"><a class="" href="/documentation">Handbuch</a></li>
+          <li class="mb-1"><a class="" href="https://github.com/codeispoetry/sharepicgenerator">Quellcode</a></li>
+          <li class="mb-1"><a class="" href="https://github.com/codeispoetry/sharepicgenerator/issues">Fehler melden</a></li>
+
         </ul>
       </div>
     </div>
+      </div>
   </footer>
 </div>
 
