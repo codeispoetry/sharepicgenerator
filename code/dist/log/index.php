dd95c8e595b815a5137c28cbb5d1b4a41553907a
Tom Rose <mail@tom-rose.de>
2022-03-18 12:08:18 +0100

    omprove logging display

--- code/dist/log/index.php
+++ code/dist/log/index.php
@@ -44,15 +44,13 @@ setlocale(LC_TIME, ' de_DE.UTF-8', 'de_DE.utf8');
                     ?>
                     gesamt: <?php echo number_format(getUsers(), 0, ',', '.'); ?>
                         <br>
+                    täglich: <?php echo number_format(getDailyUsers(), 0, ',', '.'); ?>
+                        <br>
                     heute: <?php echo number_format(getDownloadsLastDay(0), 0, ',', '.'); ?>
                         <br>
                     gestern: <?php echo number_format(getDownloadsLastDay(1), 0, ',', '.'); ?>
                         <br>
-                     vorgestern: <?php echo number_format(getDownloadsLastDay(2), 0, ',', '.'); ?>
-                        <br>
                     letzten 30 Tage: <?php echo number_format(getUsersLastDays(30), 0, ',', '.'); ?>
-                        <br>
-                    Aktivität (Median): <?php echo number_format(getUsersActivity(), 0, ',', '.'); ?>
                         <br>
                     User (1 Login, 30 Tage): <?php echo number_format(getLoginCountsPerUserLastDays('=', 1, 30), 0, ',', '.'); ?>
                         <br>
@@ -60,10 +58,7 @@ setlocale(LC_TIME, ' de_DE.UTF-8', 'de_DE.utf8');
                         <br>
                     Logzeit seit <?php echo number_format(getLoggingPeriodInDays(), 0, ',', '.'); ?> Tagen
                         <br>
-                    täglich: <?php echo number_format(getDailyUsers(), 0, ',', '.'); ?>
-                        <br>
-                    mit eigenem Logo: <?php echo getUserWithCustomLogo(); ?>
-                        <br>
+
                 </dd>
             </dl>
         </div>
@@ -77,43 +72,29 @@ setlocale(LC_TIME, ' de_DE.UTF-8', 'de_DE.utf8');
                     <br>
                     täglich: <?php echo number_format(getDailyDownloads(), 0, ',', '.'); ?>
                     <br>
-                    grünifiziert: <?php printf('%2d', 100*getGreenifyRelative()); ?>%
-                    <br>
                     für Social Media: <?php printf('%2d', 100*getSocialMedia()/$totalDownloads); ?>%
-                    <br>
-                    mit Zusatzbild: <?php printf('%2d', 100*getAddPic()/$totalDownloads); ?>%
-                        <br>
-                    mit Störer: <?php printf('%2d', 100*getWithEyecatcher()/$totalDownloads); ?>%
-                    <br>
-                    mit Sternchentext: <?php printf('%2d', 100*getAddText()/$totalDownloads); ?>%
-                    <br>
-                    mit 3-D-Effekt: <?php printf('%.1f', 100*getEraser()/$totalDownloads); ?>%
                 </dd>
             </dl>
         </div>
         <div class="col-6 col-md-6 col-lg-3">
             <dl>
-                <dt><i class="far fa-time"></i> Zeiten der letzten 7 Tage</dt>
+                <dt><i class="far fa-clock"></i> Zeiten der letzten 7 Tage</dt>
                 <dd>
                     Median Createtime: <?php echo round(getMedianCreatingTime()/1000, 1); ?>s<br>
-                    80% Createtime: <?php echo round(getMedianCreatingTime(80)/1000, 1); ?>s<br>
-                    90% Createtime: <?php echo round(getMedianCreatingTime(90)/1000, 1); ?>s<br>
-                    Mittel Createtime: <?php echo round(getAvgCreatingTime()/1000, 1); ?>s
+                    90% Createtime: <?php echo round(getMedianCreatingTime(90)/1000, 1); ?>s
                 </dd>
                 <dd>
                     Median Uploadtime: <?php echo round(getMedianUploadTime()/1000, 1); ?>s<br>
-                    80% Uploadtime: <?php echo round(getMedianUploadTime(80)/1000, 1); ?>s<br>
-
-                    Mittel Uploadtime: <?php echo round(getAvgUploadTime()/1000, 1); ?>s
+                    90% Uploadtime: <?php echo round(getMedianUploadTime(00)/1000, 1); ?>s<br>
                 </dd>
             </dl>
         </div>
-        <div class="col-6 col-md-6 col-lg-3 d-none">
+        <div class="col-6 col-md-6 col-lg-3">
             <dl>
                 <dt><i class="far fa-images"></i> Bildquellen</dt>
                 <dd>
                     <ul>
-                        <?php //showBackgroundSources(); ?>
+                        <?php showBackgroundSources(); ?>
                     </ul>
                 </dd>
             </dl>
@@ -129,12 +110,12 @@ setlocale(LC_TIME, ' de_DE.UTF-8', 'de_DE.utf8');
             </dl>
         </div>
 
-        <div class="col-6 col-md-6 col-lg-3">
+        <div class="col-6 col-md-6 col-lg-3 d-none">
             <dl>
                 <dt><i class="fas fa-sitemap"></i> Uniqe Users</dt>
                
                 
-                <dd><ul><?php echo showTenantsUniqueUsers(); ?></ul></dd>
+                <dd><ul><?php //echo showTenantsUniqueUsers(); ?></ul></dd>
 
             </dl>
         </div>
