c7dda3b680c4e54d34999b3bf6de18a7c655bfcb
Tom Rose <mail@tom-rose.de>
2022-04-06 20:07:05 +0200

    awo: fix scss

--- code/dist/tenants/cockpit/awo/eyecatcher.php
+++ code/dist/tenants/cockpit/awo/eyecatcher.php
@@ -3,21 +3,31 @@
     <div class="mb-1 list-group-item-content">
         
         <div class="d-flex align-items-lg-center">
-            <textarea name="pintext" id="pintext" placeholder="Einrichtung" class="form-control" data-maxlines="3"></textarea>
+            <textarea name="pintext" id="pintext" placeholder="Einrichtung" class="form-control" data-maxlines="5"></textarea>
         </div>
-        <div class="d-flex justify-content-end">
-            <div class="slider d-none">
-                <small>klein</small>
-                <input type="range" class="form-range" name="eyecatchersize" id="eyecatchersize" min="50"
-                    max="300" value="100">
-                <small>groß</small>
+        <div class="d-flex">
+
+            <div class="cockpit-row">
+					<div class="me-4">Größe:</div>
+
+                    <div class="slider">
+                        <span>klein</span>
+                        <input type="range" class="form-range" name="eyecatchersize" id="eyecatchersize" min="50"
+                            max="300" value="100">
+                        <span>groß</span>
+                    </div>
             </div>
-            <div>
-                <span class="cursor-pointer ms-3 text-primary align-center-eyecatcher">
-                   <i class="fab fa-centercode" title="Störer in Bildmitte"></i></span>
-                <span class="to-front" data-target="pin" title="Störer nach vorne">
-                    <i class="fas fa-layer-group text-primary"></i>
-                </span> 
+        </div>
+        <div class="d-flex">
+            <div class="cockpit-row">
+					<div class="me-4">Position:</div>
+                    <div class="d-flex">
+                        <span class="cursor-pointer me-3 text-primary align-center-eyecatcher">
+                        <i class="fab fa-centercode" title="Störer in Bildmitte"></i></span>
+                        <span class="to-front" data-target="pin" title="Störer nach vorne">
+                            <i class="fas fa-layer-group text-primary"></i>
+                        </span> 
+                    </div>
             </div>
         </div>    
     </div>
