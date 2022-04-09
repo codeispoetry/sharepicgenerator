b6b3a73b74757ce16319211f570db9215c48db65
Tom Rose <mail@tom-rose.de>
2022-04-09 12:44:52 +0200

    delete mosaic

--- code/build/js/08gridlines.js
+++ code/build/js/08gridlines.js
@@ -1,16 +1,5 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
-function showMosaicLines() {
-  for (let i = 1; i <= 2; i++) {
-    $('#canvas').append(`<div class="gridline horizontal mosaicline" style="top:${(100 * i) / 3}%;"></div>`);
-    $('#canvas').append(`<div class="gridline vertical mosaicline" style="top:0;left:${(100 * i) / 3}%;"></div>`);
-  }
-}
-
-// eslint-disable-next-line no-unused-vars
-function deleteMosaicLines() {
-  $('.mosaicline').remove();
-}
 
 $(document).ready(() => {
   if (config.user.prefs.showGridlines) {
