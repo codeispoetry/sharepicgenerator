6d5a0c12514bfc6eecc800941b26ddb78dd56e78
Thomas Rose <t.rose@seidemann-web.com>
2022-04-13 12:15:53 +0200

    introduce adfc

---
+++ code/dist/tenants/adfc/menu.php
@@ -0,0 +1,11 @@
+<div class="col d-flex">
+
+</div>
+
+<h3 class="adfcfont m-0">ADFC Sharepicgenerator</h3>
+
+<div class="col text-end text-white ptsans">
+	<a href="https://www.adfc.de/impressum.html" class="text-white" target="_blank">Impressum</a>
+		<span class="ms-2 me-2">|</span>
+	<a href="/imprint.php#privacy" class="text-white" target="_blank">Datenschutz</a>
+</div>
