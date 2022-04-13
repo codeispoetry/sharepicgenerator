29fd6f8dd590eafbbcedef5cdb4fe72fa582124d
Tom Rose <mail@tom-rose.de>
2022-04-13 15:49:47 +0200

    awo: change text

--- code/dist/tenants/awo/footer.php
+++ code/dist/tenants/awo/footer.php
@@ -9,20 +9,22 @@
                 <p>
   <img src="/assets/awo/logo-awo.png" class="d-none" style="float:right;height:80px;margin-left:20px;margin-bottom:15px">          
 Dies ist eine Mitmach-Aktion der AWO NRW im Rahmen der Kampagne "NRW gerechter machen!" 
-zur Landtagswahl 2022.<br>
-                    Für mehr Chancen-Gerechtigkeit in der Kita haben wir fünf Forderungen aufgestellt.
-<a href="https://www.awo-nrw.de/node/38648" target="_blank">Alle Details zum Kampagne finden Sie auf der Website 
-der AWO NRW</a>.<br>
+zur Landtagswahl 2022.  
+<a href="https://www.awo-nrw.de/node/38648" target="_blank"> Details dazu finden Sie auf der Website der AWO NRW.</a>.<br>
 
-Hier können Sie ein Sharepic ("Ein Bild zum Teilen") erstellen und 
-dieses herunterladen. Für die Gestaltung des Sharepics können Sie eine Forderung auswählen, 
-einen persönlichen Text verfassen und ein individuelles Bild verwenden. 
-Nach dem Download können Sie Ihr ganz persönliches Sharepic in den 
-sozialen Medien posten, um die Forderungen an die Landesregierung zu unterstützen. <br>
+Unter dem Claim "Gleich ist nicht gerecht. Mehr Chancen-Gerechtigkeit in der Kita!" wurden fünf zentrale Forderungen an die zukünftige Landesregierung aufgestellt. Beteiligen Sie sich, indem Sie hier ein Sharepic ("Ein Bild zum Teilen") erstellen, dieses herunterladen und weiterverwenden. 
+<br>
+Für die Gestaltung des Sharepics können Sie eine Forderung auswählen, einen persönlichen Text dazu verfassen und ein individuelles Bild einsetzen. Nach dem Download können Sie Ihr ganz persönliches Sharepic in den sozialen Medien posten, um die Forderungen an die Landesregierung zu unterstützen. 
+<br>
 Nutzen Sie dabei gerne auch die Hashtags #awonrw #landtagswahl2022 und #nrwgerechtermachen. 
-Wenn Sie möchten, dass die AWO Ihre Forderung nach der Wahl 
-direkt an die neue Landesregierung weitergibt, 
-dann senden Sie Ihr Sharepic gerne an <a href="MAILTO:presse@awo-owl.de">presse@awo-owl.de</a>.
+Wenn Sie möchten, dass die AWO Ihre Forderung nach der Wahl direkt an die neue Landesregierung weitergibt, 
+dann senden Sie Ihr Sharepic gerne an <a href="MAILTO:presse@awo-owl.de">presse@awo-owl.de</a>. <br>
+In der Galerie können Sie sich ansehen, was andere gestaltet haben und Ihr persönliches Sharepic auch im Rahmen der Kampagne teilen.
+
+
+
+
+.
                 </p>
 
             </div>
