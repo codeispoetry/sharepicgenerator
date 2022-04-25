ccd6df0e8639628f832cc19ca3f333dda926d50c
Thomas Rose <t.rose@seidemann-web.com>
2022-04-25 11:04:34 +0200

    fix cronjob paths

--- code/dist/cronjobs.php
+++ code/dist/cronjobs.php
@@ -1,19 +1,13 @@
 <?php
 
-require_once('base.php');
-require_once('lib/functions.php');
-require_once('lib/log_functions.php');
+$db = new SQLite3(__DIR__ . '/log/logs/log.db');
 
-$db = new SQLite3(getBasePath('log/logs/log.db'));
-
-// delete tmp-files
-deleteFilesInPathOlderThanHours(getBasePath('tmp/*'), null, 2 * 24);
-deleteFilesInPathOlderThanHours(getBasePath('tmp/*.mp4'), null, 6);
-deleteFilesInPathOlderThanHours(getBasePath('tmp/*.zip'), null, 6);
-deleteFilesInPathOlderThanHours(getBasePath('tmp/qrcode_*'), null, 2);
-deleteFilesInPathOlderThanHours(getBasePath('tmp/work*'), null, 6);
-deleteFilesInPathOlderThanHours(getBasePath('tmp/fonts/*'), null, 6);
-
+deleteFilesInPathOlderThanHours(__DIR__ . '/tmp/*', null, 2 * 24);
+deleteFilesInPathOlderThanHours(__DIR__ . '/tmp/*.mp4', null, 6);
+deleteFilesInPathOlderThanHours(__DIR__ . '/tmp/*.zip', null, 6);
+deleteFilesInPathOlderThanHours(__DIR__ . '/tmp/qrcode_*', null, 2);
+deleteFilesInPathOlderThanHours(__DIR__ . '/tmp/work*', null, 6);
+deleteFilesInPathOlderThanHours(__DIR__ . '/tmp/fonts/*', null, 6);
 
 
 $db->query(
@@ -23,3 +17,10 @@ $db->query(
 $db->query(
     'DELETE FROM "mail" WHERE timestamp <= date("now", "-1 day")'
 );
+
+
+function deleteFilesInPathOlderThanHours($path, $exclude, $hours)
+{
+    $cmd = sprintf('find %s ! -name "%s" -mmin +%d -exec rm -r {} 2> /dev/null \;', $path, $exclude, $hours * 60);
+    exec($cmd, $output);
+}
