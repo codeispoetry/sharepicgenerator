38108488291fa5aba8e13d17f38d1d14d6a51d19
Thomas Rose <t.rose@seidemann-web.com>
2022-06-22 15:58:11 +0200

    add notification when saml is back

--- code/dist/scripts/saml_check.sh
+++ code/dist/scripts/saml_check.sh
@@ -3,4 +3,5 @@ if /usr/bin/curl -I "https://saml.gruene.de" 2>&1 | grep -w "Failed" ; then
     rm /var/www/sharepicgenerator.de/shared/scripts/saml_is_up
 else
     touch /var/www/sharepicgenerator.de/shared/scripts/saml_is_up
+    php /var/www/sharepicgenerator.de/current/actions/notify.php
 fi
