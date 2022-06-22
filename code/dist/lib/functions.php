4d6422683179f1f72413c485e974c4256c3d7e5f
Thomas Rose <t.rose@seidemann-web.com>
2022-06-22 14:44:23 +0200

    refactor saml check script from double false to true

--- code/dist/lib/functions.php
+++ code/dist/lib/functions.php
@@ -273,7 +273,7 @@ function isLocalUser()
 }
 
 function with_saml(){
-    return false;
+    return file_exists('/var/www/sharepicgenerator.de/shared/scripts/saml_is_up');
 }
 
 function increaseLoginAttempts()
