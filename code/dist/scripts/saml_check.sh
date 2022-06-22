7b3d1e3cbaf13575e65249475b68aea9640088b0
Thomas Rose <t.rose@seidemann-web.com>
2022-06-22 16:10:23 +0200

    refactor samlcheck script

--- code/dist/scripts/saml_check.sh
+++ code/dist/scripts/saml_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if /usr/bin/curl -I "https://saml.gruene.de" 2>&1 | grep -w "Failed" ; then
-    rm /var/www/sharepicgenerator.de/shared/scripts/saml_is_up
+if /usr/bin/curl -I "https://saml.gruene.de" 2>&1 | grep -w "200\|301" ; then
+  touch /var/www/sharepicgenerator.de/shared/scripts/saml_is_up
+  php /var/www/sharepicgenerator.de/current/actions/notify.php
 else
-    touch /var/www/sharepicgenerator.de/shared/scripts/saml_is_up
-    php /var/www/sharepicgenerator.de/current/actions/notify.php
+    rm /var/www/sharepicgenerator.de/shared/scripts/saml_is_up
 fi
