818d673e4b422327a644660a030ad1c0d49c369a
Tom Rose <mail@tom-rose.de>
2022-07-09 16:21:24 +0200

    introduce nds

---
+++ code/build/js/nds/01init.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+const bgpic = {
+  width: 800,
+  height: 450,
+  originalWidth: 1920,
+  originalHeight: 1080,
+  filename: '/assets/bg_small.jpg',
+  fullBackgroundName: '../assets/bg.jpg',
+};
+
+var initialized = false;
+
+$(document).ready(() => {
+  $('#textsize').val(202);
+  $('#textX').val(41);
+  $('#textY').val(372);
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const clickId = urlParams.get('clickId');
+  $(`#${clickId}`).click();
+
+  if (config.user.prefs.claimtext) {
+    $('#claimtext').val(config.user.prefs.claimtext);
+    $('#claimcolor').val(config.user.prefs.claimcolor);
+  }
+
+  if (config.user.prefs.showAd && config.user.prefs.showAd === 1) {
+    $('#show-advertising').prop('checked', true);
+    ad.show();
+  } else {
+    $('#show-advertising').prop('checked', false);
+    ad.hide();
+  }
+
+  $('#addtextX').val(50);
+  $('#addtextY').val(draw.height() - 50);
+
+  showLayout();
+
+  $('.close-target').click(function doCloseTarget() {
+    $($(this).data('target')).slideUp();
+  });
+});
+
+// eslint-disable-next-line no-unused-vars
+function initSharepic() {
+  if (initialized) {
+    return false;
+  }
+  // called after background pic is loaded
+  $('#sizepresets').val('1200:1200').trigger('change');
+  initialized = true;
+
+  background.drawColor();
+
+  return true;
+}
+
+// eslint-disable-next-line no-unused-vars
+function reset() {
+  // do nothing, stay here
+  area.draw();
+}
+
+// eslint-disable-next-line no-unused-vars
+function reDraw(withAddPic = false) {
+  if (withAddPic === true) {
+    addPic1.draw();
+    addPic2.draw();
+    addPic3.draw();
+    addPic4.draw();
+    addPic5.draw();
+  }
+
+  window.setTimeout(() => {
+    pin.draw();
+    pin.drawTemplate();
+  }, 10);
+
+  window.setTimeout(() => {
+    copyright.draw();
+  }, 20);
+
+  window.setTimeout(() => {
+    addtext.draw();
+    floating.draw();
+    eraser.draw();
+    $('.align-center-text').trigger('click');
+  }, 100);
+
+  if ($('#backgroundFlipped').val() === 'true') {
+    $('#backgroundflip').click();
+  }
+}
