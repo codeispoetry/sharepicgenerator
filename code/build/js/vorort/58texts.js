03912a28942e74fd3140ae52b5d8ec244291eb0d
Thomas Rose <thomas.rose@ionos.com>
2022-09-09 11:56:29 +0200

    adjust positions

--- code/build/js/vorort/58texts.js
+++ code/build/js/vorort/58texts.js
@@ -27,13 +27,13 @@ const alltexts = {
             .attr('xml:space', 'preserve')
             .attr('style', 'white-space:pre');
     
-        const bottomY = draw.height() - 100;
+        const left1 = 30;
+        const left2 = 220;
 
         alltexts.svg3.remove();
         alltexts.svg3 = draw.text($('#text3').val())
             .font(Object.assign(alltexts.font, { size: 20 }))
             .fill('white')
-            .move(10, bottomY)
             .attr('xml:space', 'preserve')
             .attr('style', 'white-space:pre');
         
@@ -41,21 +41,30 @@ const alltexts = {
         alltexts.svg4 = draw.text($('#text4').val())
             .font(Object.assign(alltexts.font, { size: 20 }))
             .fill('white')
-            .move(200, bottomY)
             .attr('xml:space', 'preserve')
             .attr('style', 'white-space:pre');
         
         alltexts.svg5.remove();
         alltexts.svg5 = draw.text($('#text5').val())
-            .font(Object.assign(alltexts.font, { size: 10 }))
+            .font(Object.assign(alltexts.font, { size: 14 }))
             .fill('white')
-            .move(200,bottomY + 50)
             .attr('xml:space', 'preserve')
             .attr('style', 'white-space:pre');
         
 
         alltexts.svg1.move(logo.svg.x() * 0.9, logo.svg.y() - alltexts.svg1.bbox().h);
         alltexts.svg2.move(logo.svg.x() + logo.svg.width() * 0.4, logo.svg.y() + logo.svg.height() * 0.9);
+
+        
+        const rightColumnHeight = (alltexts.svg5.bbox().h == 0) ? alltexts.svg4.bbox().h : alltexts.svg4.bbox().h + alltexts.svg5.bbox().h + 10
+        const highest = Math.max( alltexts.svg3.bbox().h, rightColumnHeight);
+        const bottomY = draw.height() - highest - 30;
+
+
+        alltexts.svg3.move(left1, bottomY);
+        alltexts.svg4.move(left2, bottomY);
+        alltexts.svg5.move(left2, draw.height() - alltexts.svg5.bbox().h - 30);
+
 
     }
 }
