e4f096148468a7ef425f01b41de41bdc8841f174
Thomas Rose <thomas.rose@ionos.com>
2023-01-09 16:36:50 +0100

    add playwright tests

---
+++ code/tests/screenshots.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const fs = require('fs');
+
+
+test('take screenshot', async ({ page }) => {
+  let tenants = new Array('btw21', 'berlin','bw','einigungshilfe','nds','vorort');
+
+  for(let tenant of tenants) {
+    await page.goto(`http://localhost:9000/${tenant}/`);
+    await expect(page).toHaveScreenshot(`${tenant}.png`);
+  }
+});
