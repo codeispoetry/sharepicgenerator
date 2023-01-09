1b05fc867a28a05257d7a4280c0ce37905c5421e
Thomas Rose <thomas.rose@ionos.com>
2023-01-09 20:06:01 +0100

    changes

--- code/dist/cockpit/picture.php
+++ code/dist/cockpit/picture.php
@@ -1,7 +1,7 @@
 <h3><i class="fas fa-image"></i> Eigenes Bild</h3>
 <div class="picture  list-group-item list-group-item-action flex-column align-items-start">
     <div class="mb-1 d-flex justify-content-between">
-        <a href="#" class="btn btn-cockpit text-white text-primary cursor-pointer uploadfileclicker">
+        <a href="#" class="btn btn-cockpit text-white cursor-pointer uploadfileclicker">
             <i class="fa fa-upload"></i> Bild oder Video hochladen
         </a> 
         <input type="hidden" name="backgroundcolor" id="backgroundcolor" value="#A0C864">
@@ -63,10 +63,10 @@
         </div>
  
         <div>
-            <small class="text-primary cursor-pointer novideo me-5" id="backgroundflip"><i class="fas fa-exchange-alt"></i>
+            <small class="text-cockpit cursor-pointer novideo me-5" id="backgroundflip"><i class="fas fa-exchange-alt"></i>
                 spiegeln
             </small>
-            <small class="text-primary cursor-pointer novideo" id="backgroundreset"><i class="fas fa-undo"></i></i>
+            <small class="text-cockpit cursor-pointer novideo" id="backgroundreset"><i class="fas fa-undo"></i></i>
                 zurücksetzen
             </small>
         </div>
@@ -100,7 +100,7 @@
             <input type="range" class="form-range" name="greenifycontrast" id="greenifycontrast" min="0"
                 max="0.8" value="0.05" step="0.005">
         </div>
-        <small class="text-primary cursor-pointer novideo greenifyreset">
+        <small class="text-cockpit cursor-pointer novideo greenifyreset">
             <i class="fas fa-undo"></i> Helligkeit und Kontrast zurücksetzen
         </small>
 
