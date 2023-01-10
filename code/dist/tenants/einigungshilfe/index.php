e84b8bab8aa9add29338e70bb80328cfcdabb0e4
Thomas Rose <thomas.rose@ionos.com>
2023-01-10 20:43:59 +0100

    refactor index

--- code/dist/tenants/einigungshilfe/index.php
+++ code/dist/tenants/einigungshilfe/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('base.php');
 require_once(getBasePath("lib/functions.php"));
+require_once(getBasePath("lib/login_functions.php"));
 require_once(getBasePath("lib/save_functions.php"));
 useDeLocale();
 
@@ -8,7 +9,6 @@ session_start();
 readConfig();
 
 $landesverband = 0;
-
 $user = 'wp_einigungshilfe';
 
 $tenant = "einigungshilfe";
@@ -33,7 +33,7 @@ $_SESSION['csrf'] = $csrf;
     <meta name="theme-color" content="#46962b">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sharepicgenerator</title>
-    <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="<?php latestVersion('/assets/css/styles.css');?>">
     <link rel="stylesheet" type="text/css" href="/node_modules/bootstrap4-toggle/css/bootstrap4-toggle.min.css">
     <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicons/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicons/apple-icon-60x60.png">
@@ -48,6 +48,7 @@ $_SESSION['csrf'] = $csrf;
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicons/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png">
+    <link rel="preload" href="/assets/fonts/BereitBold-Oblique.woff2" as="font" type="font/woff2" crossorigin="anonymous">
     <link rel="manifest" href="/assets/favicons/manifest.json">
     <meta name="msapplication-TileColor" content="#46962b">
     <meta name="msapplication-TileImage" content="/assets/favicons/ms-icon-144x144.png">
@@ -65,46 +66,22 @@ $_SESSION['csrf'] = $csrf;
         config.user = {};
         config.user.prefs = {};
     </script>
-
-    <style>
-        <?php
-            $fontOptionsInCockpit = '';
-            foreach (glob("{../../persistent/fonts/" . getUser() . "*.woff2,../../assets/custom-fonts/*.woff2}", GLOB_BRACE) as $font) {
-                
-                $font_file = basename($font, '.woff2');
-                $font_family =  getFontFamily($font_file);
-                
-                printf('@font-face {
-                    font-family: "%1$s";
-                    src: url("/%3$s") format("woff2");
-                }
-                ',
-                $font_family,
-                basename($font),
-                substr($font, 6)
-                );
-
-                $fontOptionsInCockpit .= sprintf('<option value="%1$s">%2$s</option>', $font_family, $font_family);
-
-            }
-        ?>
-    </style>
 </head>
-<body class="h-100">
+<body class="h-100 d-flex flex-column text-white">
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <nav class="navbar navbar-expand-lg navbar-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" 
         data-target="#uppernavbar" aria-controls="uppernavbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-between ms-2 me-2" id="uppernavbar">
-        <?php require_once('menu.php'); ?>
+        <?php require_once('../menu.php'); ?>
     </div>
     </nav>
 </header>
-<div class="container-fluid">
-    <div class="row h-100 flex-row-reverse">
-        <div class="col-12 col-lg-9 canvas-wrapper p-0">
+<div class="container-fluid flex-grow-1">
+    <div class="row flex-row-reverse h-100">
+        <div class="col-12 col-lg-8 canvas-wrapper p-0">
             <div class="col-12 p-0 pt-3">
                 <div id="canvas-area">
                     <div id="canvas">
@@ -115,12 +92,15 @@ $_SESSION['csrf'] = $csrf;
                         <div id="grid-vertical-left" class="gridline vertical"></div>
                         <div id="grid-vertical-right" class="gridline vertical"></div>
                         <div id="grid-round" class="gridline"></div>
+                        <div id="grid-square" class="gridline gridline-square"></div>
+                        <div id="highlight-rect" class="d-none"></div>
                     </div>
                     <div class="text-center mt-5">
                         <div>
-                            <button class="btn btn-secondary btn-lg download" id="download">
+                            <button class="btn btn-secondary btn-lg download bereitbold" id="download">
                                 <i class="fas fa-download"></i> Herunterladen
                             </button>
+                            <?php displayDevelopHint(); ?>
                         </div>
                         <div id="qrcode" class="qrcode mt-5" style="display:none">
                             Du kannst Dein Sharepic auch auf Dein Handy herunterladen.<br>
@@ -137,9 +117,8 @@ $_SESSION['csrf'] = $csrf;
             require_once(getBasePath('/lib/overlays/waiting.php'));
            
             require_once(getBasePath('/lib/overlays/imagedb.php'));
-
-            require_once('preferences.php');
-            require_once('overlays/faq.php');
+            require_once(getBasePath('/lib/overlays/preferences.php'));
+            require_once(getBasePath('/lib/overlays/faq.php'));
 
 
             ?>
@@ -147,10 +126,11 @@ $_SESSION['csrf'] = $csrf;
             <div class="col-12 mt-3 mb-3">
                 <div id="message" class="bg-danger text-white p-4" style="display:none"></div>
                 <div id="warning" class="text-danger text-center p-4" style="display:none">Gesicht</div>
+               
             </div>
 
             </div>
-        <div class="col-12 col-lg-3 p-0">
+        <div class="col-12 col-lg-4 p-0">
             <div class="cockpit h-100">
                 <?php require_once('cockpit.php'); ?>
             </div> 
@@ -159,6 +139,7 @@ $_SESSION['csrf'] = $csrf;
 </div>
 
 <?php require_once('../footer.php'); ?>
+
 <script src="/node_modules/jquery/dist/jquery.min.js"></script>
 <script src="/node_modules/popper.js/dist/umd/popper.min.js"></script>
 <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -167,8 +148,8 @@ $_SESSION['csrf'] = $csrf;
 <script src="/node_modules/@svgdotjs/svg.draggable.js/dist/svg.draggable.min.js"></script>
 <script src="/node_modules/@svgdotjs/svg.filter.js/dist/svg.filter.min.js"></script>
 
-<script src="/assets/js/main.min.js?v=<?php echo @filemtime('../../assets/js/main.min.js'); ?>"></script>
-<script src="/assets/js/einigungshilfe.min.js?v=<?php echo @filemtime('../../assets/js/einigungshilfe.min.js'); ?>"></script>
+<script src="<?php latestVersion('/assets/js/main.min.js');?>"></script>
+<script src="<?php latestVersion('/assets/js/einigungshilfe.min.js');?>"></script>
 
 
 
@@ -187,10 +168,6 @@ if (isset($_GET['usePicture'])) {
 ?>
     config.user.prefs = jQuery.parseJSON('<?php echo getUserPrefs(); ?>');
     config.username = '<?php echo getUser(); ?>';
-
-    window.setTimeout(
-        () => { $('input[value=<?php echo getSaying('layout'); ?>').click(); },
-    150);
 </script>
 
 </body>
