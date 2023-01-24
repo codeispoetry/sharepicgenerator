e0063e18793a8c7d2b90c76a5cfc643949f9cec1
Thomas Rose <thomas.rose@ionos.com>
2023-01-24 08:45:30 +0100

    delete max lines

--- code/build/js/55maxlines.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-$('[data-maxlines]').bind('input propertychange', reduceLines);
-
-function reduceLines() {
-  const text = $(this).val().split(/\n/, $(this).data('maxlines'));
-  $(this).val(text.join('\n'));
-}
