20c7e9419581a75b91f68e932a855f94b613bf25
Thomas Rose <thomas.rose@ionos.com>
2023-02-01 09:33:20 +0100

    add tenant handling on cli

--- scripts/delete-tenant.sh
+++ scripts/delete-tenant.sh
@@ -1,10 +1,10 @@
-if [ ! -d code/dist/tenants/$1 ]
+if [ ! -d code/dist/tenants/cockpit/$1 ]
 then
   echo $1 does not exist. Abort
   exit
 fi
 
-rm -rf code/dist/tenants/$1
+rm -rf code/dist/tenants/cockpit/$1
 rm -rf code/build/js/$1
 rm -rf code/dist/assets/$1
 
