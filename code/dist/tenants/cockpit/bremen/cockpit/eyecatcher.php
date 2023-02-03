e8cfd16223b98aff707d60c76fee849835c33984
Thomas Rose <thomas.rose@ionos.com>
2023-02-03 10:25:33 +0100

    add bremen

---
+++ code/dist/tenants/cockpit/bremen/cockpit/eyecatcher.php
@@ -0,0 +1,30 @@
+<h3>Störer</h3>
+<div class="eyecatcher list-group-item list-group-item-action flex-column align-items-start">
+    <div class="mb-1 list-group-item-content">
+        <div class="d-none">
+            <select class="form-select" name="eyecatchertemplate" id="eyecatchertemplate">
+                <option value="">Vorlage wählen</option>
+                <option value="custom">Text selbst eingeben</option>
+                <optgroup label="Vorlagen">
+                    <option value="btw21/zweitstimme.svg">Zweitstimme Grün!</option>
+                    <option value="btw21/briefwahl.svg">Briefwahl jetzt!</option>
+                </optgroup>
+            </select>
+            oder
+        </div>
+        <div class="d-flex align-items-lg-center">
+            <textarea name="pintext" id="pintext" placeholder="Dein Text" class="form-control" data-maxlines="3"></textarea>
+        </div>
+        <div class="d-flex justify-content-start">
+            <div class="slider advancedmode d-none">
+                <small>klein</small>
+                <input type="range" class="form-range" name="eyecatchersize" id="eyecatchersize" min="50"
+                    max="300" value="100">
+                <small>groß</small>
+            </div>
+        </div>    
+    </div>
+</div>
+
+<input type="hidden" name="pinX" id="pinX">
+<input type="hidden" name="pinY" id="pinY">
