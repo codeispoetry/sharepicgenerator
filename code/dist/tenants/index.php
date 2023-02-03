646b0fb05e0314e5dbf1fc8c2cc09b6bd0be807a
Thomas Rose <thomas.rose@ionos.com>
2023-02-03 09:36:42 +0100

    add save button

--- code/dist/tenants/index.php
+++ code/dist/tenants/index.php
@@ -85,7 +85,10 @@ $_SESSION['csrf'] = $csrf;
                     </div>
                     <div class="d-flex justify-content-around mt-5">
                         <div class="w-50 text-center">
-                            <div class="text-center">
+                            <div class="">
+                                <button class="btn btn-outline-secondary btn-sm save bereitbold me-2" id="save">
+                                    <i class="fas fa-save"></i> <?php _e("Save"); ?>
+                                </button>
                                 <button class="btn btn-secondary btn-lg download bereitbold" id="download">
                                     <i class="fas fa-download"></i> <?php _e("Download"); ?>
                                 </button>
