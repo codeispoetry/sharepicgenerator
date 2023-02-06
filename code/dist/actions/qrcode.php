c6142b8ea5158287390edf5ef83cbedfcee81aa7
Thomas Rose <thomas.rose@ionos.com>
2023-02-06 22:59:21 +0100

    do not log qrcode

--- code/dist/actions/qrcode.php
+++ code/dist/actions/qrcode.php
@@ -3,17 +3,17 @@ require_once('base.php');
 require_once(getBasePath('lib/functions.php'));
 $file = sprintf('%s/%s', getBasePath('tmp'), sanitizeUserinput($_GET['f']));
 
-$db = new SQLite3(getBasePath('log/logs/log.db'));
-if (isAdmin()) {
-    $db->exec('CREATE TABLE IF NOT EXISTS qrcode(
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            url TEXT)');
-}
+// $db = new SQLite3(getBasePath('log/logs/log.db'));
+// if (isAdmin()) {
+//     $db->exec('CREATE TABLE IF NOT EXISTS qrcode(
+//             id INTEGER PRIMARY KEY AUTOINCREMENT, 
+//             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+//             url TEXT)');
+// }
 
-$smt = $db->prepare('INSERT INTO qrcode (url) values (:file)');
-$smt->bindValue(':url', $file, SQLITE3_TEXT);
-$smt->execute();
+// $smt = $db->prepare('INSERT INTO qrcode (url) values (:file)');
+// $smt->bindValue(':url', $file, SQLITE3_TEXT);
+// $smt->execute();
 
 header('Content-Type: image/png');
 header("Content-Length: ".filesize($file));
