d4e8db7099b44cd25545efb497c60f1403353181
Thomas Rose <thomas.rose@ionos.com>
2023-02-07 00:13:48 +0100

    add ai log

--- code/dist/lib/overlays/blog.php
+++ code/dist/lib/overlays/blog.php
@@ -1,11 +1,11 @@
 <div class="col-12">
-    <div id="blog" class="overlay pe-3"  style="display:none;">
+    <div id="blog" class="overlay pe-3" style="display:none;">
         <a href="#" class="close closer text-danger" data-bs-target="#blog">
             <i class="fas fa-times"></i>
         </a>
         <h2>Dieser Sharepicgenerator ist neu!</h2>
-       Das ist neu:
-       <ol>
+        Das ist neu:
+        <ol>
             <li>Lass Dir alternative Textvorschläge einer Künstlichen Intelligenz unterbreiten</li>
             <li>Dein letztes Sharepic wird beim nächsten Einloggen automatisch wieder geladen</li>
             <li>Lade eigene Bilder auch per Drag-and-Drop hoch</li>
@@ -13,9 +13,10 @@
             <li>Wähle die Farbe Deiner Texte</li>
             <li>Steuere die Größe von Logos und Störer selbst</li>
             <li>und viele kleinere Verbesserungen mehr</li>
-       </ol>
-       <a href="#" class="closer text-danger me-3" data-bs-target="#blog">
-            schließen
+        </ol>
+        <h2>Viel Spaß damit!</h2>
+        <a href="#" class="closer btn btn-lg btn-pistazie mt-3 mb-3" data-bs-target="#blog">
+            Jetzt Sharepic erstellen
         </a>
     </div>
 </div>
