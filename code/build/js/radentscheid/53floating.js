7ad18dbb67ea5cd46a62e546afd23ca7182b4552
Thomas Rose <thomas.rose@ionos.com>
2023-02-08 08:57:36 +0100

    three texts radentscheid

--- code/build/js/radentscheid/53floating.js
+++ code/build/js/radentscheid/53floating.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
-var inFloatingDraw = 0;
 const floating = {
-  svg: draw.text(''),
+  svg1: draw.text(''),
+  svg2: draw.text(''),
+  svg3: draw.text(''),
   fond: draw.circle(0),
   fondPadding: 30,
   align: 'left',
@@ -11,209 +12,42 @@ const floating = {
     leading: '1.05em',
     size: 20,
   },
-  fontBefore: {
-    family: 'Bebas Neue',
-    anchor: 'left',
-    leading: '1.05em',
-    size: 10,
-  },
-  fontCiteSymbol: {
-    family: 'Bebas Neue',
-    anchor: 'left',
-    leading: '1.05em',
-    size: 80,
-  },
-  fontAfter: {
-    family: 'Bebas Neue',
-    anchor: 'left',
-    leading: '1.05em',
-    size: 10,
-  },
 
-  draw() {
-    if ($('#text').val() === '') {
-      return;
+  draw(i = undefined) {
+    if( isNaN(i) ) {
+      i = $(this).attr('id')?.substr(-1) || 1;
     }
 
-    floating.svg.remove();
-    floating.svg = draw.group().addClass('draggable').draggable();
+    floating[`svg${i}`].remove();
+    floating[`svg${i}`] = draw.group().addClass('draggable').draggable();
 
-    floating.svg.on('dragend.namespace', function floatingDragEnd() {
-      $('#textX').val(Math.round(this.x()));
-      $('#textY').val(Math.round(this.y()));
+    floating[`svg${i}`].on('dragend.namespace', function floatingDragEnd() {
+      $('#textX' + i).val(Math.round(this.x()));
+      $('#textY' + i).val(Math.round(this.y()));
     });
 
     const anchor = floating.align;
-
-    const t = draw.text($('#text').val())
+    const t = draw.text($(`#text${i}`).val())
       .font(Object.assign(floating.font, { anchor }))
-      .fill($('#textcolor').val())
+      .fill($(`#textcolor${i}`).val())
       .attr('xml:space', 'preserve')
       .attr('style', 'white-space:pre');
 
-    if ($('#textShadow').prop('checked')) {
-      t.filterWith((add) => {
-        const blur = add.offset(0, 0).in(add.$sourceAlpha).gaussianBlur(0.5);
-        add.blend(add.$source, blur);
-      });
-    }
+    floating[`svg${i}`].add(t);   
 
-    if ($('#textafter').val()) {
-      floating.svg.add(floating.drawTextAfter(t));
-    }
+    floating[`svg${i}`].move($('#textX' + i).val(), $('#textY' + i).val());
+    floating[`svg${i}`].front();
 
-    floating.svg.add(t);
-
-    floating.drawClaim(t);
-
-    if ($('#textbefore').val() || $('#layout-cite').prop('checked')) {
-      floating.svg.add(floating.drawTextBefore());
-    }
-
-    floating.svg
-      .move($('#textX').val(), $('#textY').val())
-      .size(parseInt($('#textsize').val(), 10));
-
-    floating.svg.front();
   },
 
-  drawClaim(t) {
-    if ($('#claimtext').val() === '') {
-      return;
+  scale() {
+    for(let i = 1; i <= 3; i++) {
+      floating[`svg${i}`]
+        .scale($(this).data('scale'));
     }
-
-    const claim = draw.group();
-
-    let textColor = '#FFFFFF';
-    if ($('#claimcolor').val() === '#ffe100') {
-      textColor = '#145f32';
-    }
-
-    const textInClaim = $('#claimtext').val();
-
-    const claimText = draw.text(textInClaim)
-      .font({
-        family: 'BereitBold',
-        anchor: 'left',
-        leading: '1.05em',
-        size: 8,
-      })
-      .move(2, 1)
-      .fill(textColor);
-
-    // if (textInClaim.includes('Ä') || textInClaim.includes('Ö') || textInClaim.includes('Ü')) {
-
-    const claimBackground = draw.rect(claimText.bbox().w + 4, 11.5)
-      .fill($('#claimcolor').val())
-      .skew(-8, 0)
-      .addTo(claim);
-
-    claimText.addTo(claim);
-
-    let x;
-    switch (floating.align) {
-      case 'middle':
-        x = -claim.width() / 2;
-        break;
-      case 'end':
-        x = -claim.width();
-        break;
-      default:
-        x = 0;
-    }
-
-    claim.move(x, 8 + floating.svg.height() + 1 - 20);
-
-    claim.addTo(floating.svg);
   },
 
-  drawTextBefore() {
-    let content = $('#textbefore').val();
-    let color = '#FFE100';
-    let font = floating.fontBefore;
-
-    if ($('#layout-cite').prop('checked')) {
-      content = ',,';
-      color = '#FFFFFF';
-      font = floating.fontCiteSymbol;
-    }
-
-    const textbefore = draw.text(content)
-      .font(font)
-      .fill(color)
-      .attr('xml:space', 'preserve')
-      .attr('style', 'white-space:pre');
-
-    if ($('#textShadow').prop('checked')) {
-      textbefore.filterWith((add) => {
-        const blur = add.offset(0, 0).in(add.$sourceAlpha).gaussianBlur(0.5);
-        add.blend(add.$source, blur);
-      });
-    }
-
-    textbefore.move(0, -20 - textbefore.bbox().h);
-
-    switch (floating.align) {
-      case 'middle':
-        textbefore.x(-textbefore.bbox().w / 2);
-        break;
-      case 'end':
-        textbefore.x(-textbefore.bbox().w);
-        break;
-      default:
-    }
-
-    return textbefore;
-  },
-
-  drawTextAfter(t) {
-   // claim.svg.hide();
-
-    const textafter = draw.text($('#textafter').val())
-      .font(floating.fontAfter)
-      .fill('#FFFFFF')
-      .move(0, -20 + t.bbox().height)
-      .attr('xml:space', 'preserve')
-      .attr('style', 'white-space:pre');
-
-    if ($('#textShadow').prop('checked')) {
-      textafter.filterWith((add) => {
-        const blur = add.offset(0, 0).in(add.$sourceAlpha).gaussianBlur(0.5);
-        add.blend(add.$source, blur);
-      });
-    }
-
-    switch (floating.align) {
-      case 'middle':
-        textafter.x(-textafter.bbox().w / 2);
-        break;
-      case 'end':
-        textafter.x(-textafter.bbox().w);
-        break;
-      default:
-    }
-
-    return textafter;
-  },
-
-  hide() {
-    floating.svg.hide();
-  },
-
-  setAlign() {
-    floating.align = $(this).data('align');
-    floating.draw();
-  },
 };
+$('.text-trigger').bind('input propertychange', floating.draw);
+$('.textscale').click(floating.scale);
 
-$('#text, #textafter, #textbefore, #textsize, #showclaim, #claimtext, #textShadow, #textcolor').bind('input propertychange', floating.draw);
-$('.text-align').click(floating.setAlign);
-
-$('.align-center-text').click(() => {
-  const scaleFactor = parseInt($('#textsize').val(), 10) / 100;
-  const textWidth   = floating.svg.width()  * scaleFactor;
-  const textHeight  = floating.svg.height() * scaleFactor;
-  $('#textX').val((draw.width() - textWidth) / 2);
-  $('#textY').val((draw.height() - textHeight) / 2);
-  floating.draw();
-});
--- code/dist/tenants/cockpit/radentscheid/cockpit/text.php
+++ code/dist/tenants/cockpit/radentscheid/cockpit/text.php
@@ -1,55 +1,37 @@
 
 
-        <h3>Text</h3>
-        <div class="text list-group-item list-group-item-action flex-column align-items-start">
-            
-            <div class="d-block list-group-item-content">
-               
-                <div class="d-none align-items-lg-center">
-                    <input type="text" placeholder="Text darüber" name="textbefore" id="textbefore" value="" class="form-control">
-                </div>
-                <div class="d-flex">
-                    <textarea placeholder="Haupttext" name="text" id="text" class="form-control">Sicher
-Radeln
-Jetzt!</textarea>
-
-                    <input type="hidden" name="textcolor" id="textcolor" value="<?php getColorAtIndex(2); ?>">
-                    <span 
-                        class="colorpicker ms-1"  
-                        id="textcolorpicker" 
-                        data-colors="<?php getColorAtIndex(); ?>" 
-                        data-action="floating.draw()" 
-                        data-field="#textcolor" 
-                        title="Farbe wechseln"></span> 
-                </div>
-                <div class="d-none align-items-lg-center">
-                    <textarea placeholder="Text unter der Linie" name="textafter" id="textafter" value="" class="form-control h-1em"></textarea>
-                </div>
-                <div class="d-none justify-content-between">
-                    <div class="">
-                        <input type="text" placeholder="Claim" name="claimtext" id="claimtext" value="" class="form-control">
-                    </div>    
-                    <div>
-                        <input type="hidden" name="claimcolor" id="claimcolor" value="#e2ba00">
-                        <span class="colorpicker ms-1"  id="claimcolorpicker" data-colors="#1a1a18,#008fcf,#e2ba00,#ffffff" data-action="floating.draw()" data-field="#claimcolor" title="Farbe wechseln"></span>
-                    </div>    
-                </div>
-
-                <div class="mb-1 mt-2">
-                    <div class="d-flex justify-content-between">
-                        <div class="slider">
-                            <small>klein</small>
-                            <input type="range" class="form-range" name="textsize" id="textsize" min="1" max="100">
-                            <small>groß</small>
-                        </div>
-
-                    </div> 
-                    </div>
-                
-            </div>
+<h3>Text</h3>
+<div class="text list-group-item list-group-item-action flex-column align-items-start">
+    
+    <div class="d-block list-group-item-content">
+        
+        <div class="d-none align-items-lg-center">
+            <input type="text" placeholder="Text darüber" name="textbefore" id="textbefore" value="" class="form-control">
         </div>
-        <input type="hidden" name="iconfile" id="iconfile">
-        <input type="hidden" name="textX" id="textX">
-        <input type="hidden" name="textY" id="textY">
-        <input type="hidden" name="textColor" id="textColor" value="0">
-        <input type="file" class="custom-file-input upload-file" id="uploadicon" accept="image/*">
+        <?php for ($i=1; $i <= 3; $i++) {
+            $texts = array(null, 'Sicher', 'radeln.', 'Jetzt!');
+            ?>
+        <div class="d-flex">
+            <textarea placeholder="Text" name="text<?php echo $i; ?>" id="text<?php echo $i; ?>" class="form-control text-trigger"><?php echo $texts[$i]; ?></textarea>
+            <input type="hidden" name="textX<?php echo $i; ?>" id="textX<?php echo $i; ?>">
+            <input type="hidden" name="textY<?php echo $i; ?>" id="textY<?php echo $i; ?>">
+            <input type="hidden" name="textcolor<?php echo $i; ?>" id="textcolor<?php echo $i; ?>" value="<?php getColorAtIndex(2); ?>">
+            <span 
+                class="colorpicker ms-1"  sd
+                id="textcolorpicker" 
+                data-colors="<?php getColorAtIndex(); ?>" 
+                data-action="floating.draw(<?php echo $i; ?>)" 
+                data-field="#textcolor<?php echo $i; ?>" 
+                title="Farbe wechseln"></span> 
+        </div>
+        <?php } ?>
+
+        <div class="mb-1 mt-2">
+            <div class="d-flex justify-content-between">
+                <a class="btn btn-sm btn-outline-cockpit textscale" data-scale="0.9">kleiner</a>
+                <span class="btn btn-sm btn-outline-cockpit textscale" data-scale="1.1">größer</span>
+            </div> 
+            </div>
+        
+    </div>
+</div>
