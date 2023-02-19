b0b8862b7cfa72a946b69ac53af2cee2ea8f5b9d
Thomas Rose <thomas.rose@ionos.com>
2023-02-19 01:54:27 +0100

    add font class names

--- code/dist/tenants/fonts.php
+++ code/dist/tenants/fonts.php
@@ -10,5 +10,11 @@ foreach ($fonts as $font) {
         '@font-face {font-family: "%1$s";src: url("../assets/typefaces/%1$s.woff2") format("woff2");}',
         basename($font, '.woff2')
     );
+
+    printf(
+        ".%s{font-family: '%s';font-weight: 500;}",
+        strToLower(preg_replace('/ /', '', basename($font, '.woff2'))),
+        basename($font, '.woff2')
+    );
 }
 echo '</style>';
