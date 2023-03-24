84e00a7d6ce63e41a1f8a778728bb87fb8aa237d
Thomas Rose <thomas.rose@ionos.com>
2023-03-24 14:08:18 +0100

    add vignette

--- code/build/js/bayern/72frame.js
+++ code/build/js/bayern/72frame.js
@@ -5,15 +5,28 @@ const frame = {
 
         frame.svg.remove();
 
-        const t = draw.width() * $('#framewidth').val() / 1080; // thickness of frame
+        let t = draw.width() * $('#framewidth').val() / 1080; // thickness of frame
 
         const w = draw.width();
         const h = draw.height();
+        this.svg = draw.group();
 
-        this.svg = draw.polygon(`
+        const outer = draw.polygon(`
            0,0 ${w},0 ${w},${h} 0,${h}
            0, ${t} ${t},${t} ${t},${h - t} ${w - t}, ${h - t} ${w - t}, ${t}, 0, ${t}
             `).fill('#8abf2c');
+
+        t = t * 1.1;
+        const shadow = draw.polygon(`
+            0,0 ${w},0 ${w},${h} 0,${h}
+            0, ${t} ${t},${t} ${t},${h - t} ${w - t}, ${h - t} ${w - t}, ${t}, 0, ${t}
+             `).fill('#000').opacity(0.9);
+        shadow.filterWith(function(add) {
+            add.gaussianBlur(30)
+        })
+
+        this.svg.add(shadow);
+        this.svg.add(outer);
 
         frame.svg.front();
         bayernlogo.svg.front();
