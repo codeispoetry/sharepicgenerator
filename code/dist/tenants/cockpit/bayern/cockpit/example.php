a67359149c6c9a84077062e0be90f457af109e71
Thomas Rose <thomas.rose@ionos.com>
2023-05-19 09:19:10 +0200

    add link to selfpublish

--- code/dist/tenants/cockpit/bayern/cockpit/example.php
+++ code/dist/tenants/cockpit/bayern/cockpit/example.php
@@ -1,11 +1,7 @@
 <h3 class="">Beispiel</h3>
 <div class="list-group-item list-group-item-action flex-column align-items-start">
+    <img src="/assets/bayern/example.jpg" class="img-fluid float-start me-1 w-25" alt="Beispiel">
     Hier siehst Du ein Beispiel für ein Sharepic mit freigestelltem Bild.
     Mehr Beispiele findest Du auf unserem 
-    <a href="https://www.instagram.com/gruenebayern/" target="_blank">Instagram-Account</a>.
-    
-    <div>
-        <img src="/assets/bayern/example.jpg" class="img-fluid w-25" alt="Beispiel">
-    </div>
-  
+    <a href="https://www.instagram.com/gruenebayern/" target="_blank">Instagram-Account</a>.  
 </div>
